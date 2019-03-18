e3cdfef779dc08743d5b5e0eb463065690023beb
Yong Sheng Tan <yong.tan@symphony.com>
2019-03-18 11:29:16 +0800

    Fix assumption that store paths starting with "/" load as resources

--- src/main/java/utils/HttpClientBuilderHelper.java
+++ src/main/java/utils/HttpClientBuilderHelper.java
@@ -1,89 +1,67 @@
 package utils;
 
 import configuration.SymConfig;
-import configuration.SymConfigLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import javax.ws.rs.client.ClientBuilder;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 
 public class HttpClientBuilderHelper {
-
     private final static Logger logger = LoggerFactory.getLogger(HttpClientBuilderHelper.class);
 
-    public static ClientBuilder getHttpClientBuilderWithTruststore (SymConfig config){
-        KeyStore tks = null;
-        try {
-            tks = KeyStore.getInstance("JKS");
-        } catch (KeyStoreException e) {
-            logger.error("getHttpClientBuilderWithTruststore", e);
-        }
-
+    public static ClientBuilder getHttpClientBuilderWithTruststore(SymConfig config) {
+        KeyStore jksStore = getJksKeystore();
         ClientBuilder clientBuilder = ClientBuilder.newBuilder();
 
-        if (config.getTruststorePath() != null) {
-            loadTrustStore(config, tks, clientBuilder);
+        if (config.getTruststorePath() != null && jksStore != null) {
+            loadTrustStore(config, jksStore, clientBuilder);
         }
-
         return clientBuilder;
     }
 
-    public static ClientBuilder getHttpClientBotBuilder (SymConfig config){
-        KeyStore cks = null;
-        KeyStore tks = null;
-        try {
-            cks = KeyStore.getInstance("PKCS12");
-            tks = KeyStore.getInstance("JKS");
-        } catch (KeyStoreException e) {
-            logger.error("Error creating keystore instance", e);
-        }
+    public static ClientBuilder getHttpClientBotBuilder(SymConfig config) {
+        KeyStore pkcsStore = getPkcsKeystore();
+        KeyStore jksStore = getJksKeystore();
 
-        try (InputStream keyStoreIS = loadInputStream(config.getBotCertPath()+config.getBotCertName())) {
-            cks.load(keyStoreIS,config.getBotCertPassword().toCharArray());
+        try (InputStream keyStoreIS = loadInputStream(config.getBotCertPath() + config.getBotCertName())) {
+            if (pkcsStore != null) {
+                pkcsStore.load(keyStoreIS, config.getBotCertPassword().toCharArray());
+            }
         } catch (CertificateException | NoSuchAlgorithmException | IOException e) {
             logger.error("Error loading bot keystore file", e);
         }
 
-        ClientBuilder clientBuilder = ClientBuilder.newBuilder().keyStore(cks, config.getBotCertPassword().toCharArray());
-        if (config.getTruststorePath() != null) {
-            loadTrustStore(config, tks, clientBuilder);
+        ClientBuilder clientBuilder = ClientBuilder.newBuilder().keyStore(pkcsStore, config.getBotCertPassword().toCharArray());
+        if (config.getTruststorePath() != null && jksStore != null) {
+            loadTrustStore(config, jksStore, clientBuilder);
         }
         return clientBuilder;
     }
 
-    public static ClientBuilder getHttpClientAppBuilder (SymConfig config){
-        KeyStore cks = null;
-        KeyStore tks = null;
-        try {
-            cks = KeyStore.getInstance("PKCS12");
-            tks = KeyStore.getInstance("JKS");
-        } catch (KeyStoreException e) {
-            logger.error("Error creating keystore instance", e);
-        }
+    public static ClientBuilder getHttpClientAppBuilder(SymConfig config) {
+        KeyStore pkcsStore = getPkcsKeystore();
+        KeyStore jksStore = getJksKeystore();
 
-        try (InputStream keyStoreIS = loadInputStream(config.getAppCertPath()+config.getAppCertName())) {
-            cks.load(keyStoreIS,config.getBotCertPassword().toCharArray());
+        try (InputStream keyStoreIS = loadInputStream(config.getAppCertPath() + config.getAppCertName())) {
+            if (pkcsStore != null) {
+                pkcsStore.load(keyStoreIS, config.getBotCertPassword().toCharArray());
+            }
         } catch (CertificateException | NoSuchAlgorithmException | IOException e) {
             logger.error("Error loading app keystore file", e);
         }
 
-        ClientBuilder clientBuilder = ClientBuilder.newBuilder().keyStore(cks, config.getAppCertPassword().toCharArray());
-        if(config.getTruststorePath()!=null){
-            loadTrustStore(config, tks, clientBuilder);
+        ClientBuilder clientBuilder = ClientBuilder.newBuilder().keyStore(pkcsStore, config.getAppCertPassword().toCharArray());
+        if (config.getTruststorePath() != null && jksStore != null) {
+            loadTrustStore(config, jksStore, clientBuilder);
         }
         return clientBuilder;
     }
 
     private static void loadTrustStore(SymConfig config, KeyStore tks, ClientBuilder clientBuilder) {
-        //load truststore
         try (InputStream trustStoreIS = loadInputStream(config.getTruststorePath())) {
             tks.load(trustStoreIS, config.getTruststorePassword().toCharArray());
             clientBuilder.trustStore(tks);
@@ -92,11 +70,31 @@ public class HttpClientBuilderHelper {
         }
     }
 
-    private static InputStream loadInputStream(String fileName) throws FileNotFoundException{
-        if (fileName.startsWith("/")) {
+    private static InputStream loadInputStream(String fileName) throws FileNotFoundException {
+        if ((new File(fileName)).exists()) {
+            return new FileInputStream(fileName);
+        } else if (HttpClientBuilderHelper.class.getResource(fileName) != null) {
             return HttpClientBuilderHelper.class.getResourceAsStream(fileName);
         } else {
-            return new FileInputStream(fileName);
+            throw new FileNotFoundException();
         }
+    }
+
+    private static KeyStore getPkcsKeystore() {
+        try {
+            return KeyStore.getInstance("PKCS12");
+        } catch (KeyStoreException e) {
+            logger.error("Error creating PKCS keystore instance", e);
+        }
+        return null;
+    }
+
+    private static KeyStore getJksKeystore() {
+        try {
+            return KeyStore.getInstance("JKS");
+        } catch (KeyStoreException e) {
+            logger.error("Error creating JKS keystore instance", e);
+        }
+        return null;
     }
 }
