5a04b13439f01ede7e7aaed1dd58896870fbac60
Yong Sheng Tan <yong.tan@symphony.com>
2019-04-03 17:00:14 +0800

    Add constructors for OutboundMessage

--- src/main/java/model/OutboundMessage.java
+++ src/main/java/model/OutboundMessage.java
@@ -1,14 +1,31 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import java.io.File;
-@JsonIgnoreProperties(ignoreUnknown = true)
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OutboundMessage {
-    String message;
-    String data;
-    File[] attachment;
+    private String message;
+    private String data;
+    private File[] attachment;
+
+    public OutboundMessage() {
+    }
+
+    public OutboundMessage(String message) {
+        this.message = message;
+    }
+
+    public OutboundMessage(String message, String data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public OutboundMessage(String message, String data, File[] attachment) {
+        this.message = message;
+        this.data = data;
+        this.attachment = attachment;
+    }
 
     public String getMessage() {
         return message;
