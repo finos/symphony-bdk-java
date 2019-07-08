e08c3fe6e03c0c6de889c56627d4f9160f6afd5b
Yong Sheng Tan <yong.tan@symphony.com>
2019-07-08 16:19:25 +0800

    Simplified bootstrap

--- src/main/java/listeners/RoomListener.java
+++ src/main/java/listeners/RoomListener.java
@@ -4,23 +4,14 @@ import model.InboundMessage;
 import model.Stream;
 import model.events.*;
 
-public interface RoomListener {
-
+public interface RoomListener extends DatafeedListener {
     void onRoomMessage(InboundMessage message);
-
     void onRoomCreated(RoomCreated roomCreated);
-
     void onRoomDeactivated(RoomDeactivated roomDeactivated);
-
     void onRoomMemberDemotedFromOwner(RoomMemberDemotedFromOwner roomMemberDemotedFromOwner);
-
     void onRoomMemberPromotedToOwner(RoomMemberPromotedToOwner roomMemberPromotedToOwner);
-
     void onRoomReactivated(Stream stream);
-
     void onRoomUpdated(RoomUpdated roomUpdated);
-
     void onUserJoinedRoom(UserJoinedRoom userJoinedRoom);
-
     void onUserLeftRoom(UserLeftRoom userLeftRoom);
 }
