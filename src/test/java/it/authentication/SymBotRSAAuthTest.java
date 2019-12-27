3395061e1356b2dff95cf04f1e689f812fc87256
Yong Sheng Tan <yong.tan@symphony.com>
2019-12-27 15:21:11 +0800

    Ignore SSL verification in integration tests for mock server

--- src/test/java/it/authentication/SymBotRSAAuthTest.java
+++ src/test/java/it/authentication/SymBotRSAAuthTest.java
@@ -1,7 +1,6 @@
 package it.authentication;
 
 import authentication.AuthEndpointConstants;
-import authentication.ISymAuth;
 import authentication.SymBotRSAAuth;
 import exceptions.AuthenticationException;
 import it.commons.ServerTest;
@@ -12,33 +11,33 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.Assert.*;
 
 public class SymBotRSAAuthTest extends ServerTest {
-  @Test
-  public void authenticateSuccess() {
-    stubFor(post(urlEqualTo(AuthEndpointConstants.SESSION_AUTH_PATH_RSA))
-        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-        .willReturn(aResponse()
-            .withStatus(200)
-            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-            .withBody("{ \"token\": \"eyJhbGciOiJSUzUxMiJ97oqG1Kd28l1FpQ\", \"name\": \"sessionToken\" }")));
+    @Test
+    public void authenticateSuccess() {
+        stubFor(post(urlEqualTo(AuthEndpointConstants.SESSION_AUTH_PATH_RSA))
+            .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .withBody("{ \"token\": \"eyJhbGciOiJSUzUxMiJ97oqG1Kd28l1FpQ\", \"name\": \"sessionToken\" }")));
 
-    stubFor(post(urlEqualTo(AuthEndpointConstants.KEY_AUTH_PATH_RSA))
-        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-        .willReturn(aResponse()
-            .withStatus(200)
-            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-            .withBody("{ \"token\": \"0100e4feOiJSUzUxMiJ97oqGf729d1866f\", \"name\": \"keyManagerToken\" }")));
+        stubFor(post(urlEqualTo(AuthEndpointConstants.KEY_AUTH_PATH_RSA))
+            .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .withBody("{ \"token\": \"0100e4feOiJSUzUxMiJ97oqGf729d1866f\", \"name\": \"keyManagerToken\" }")));
 
-    ISymAuth symBotRSAAuth = new SymBotRSAAuth(config);
+        SymBotRSAAuth symBotRSAAuth = new SymBotRSAAuth(config);
 
-      try {
-          symBotRSAAuth.authenticate();
+        try {
+            symBotRSAAuth.authenticate();
 
-          assertNotNull(symBotRSAAuth.getSessionToken());
-          assertEquals("eyJhbGciOiJSUzUxMiJ97oqG1Kd28l1FpQ", symBotRSAAuth.getSessionToken());
-          assertNotNull(symBotRSAAuth.getKmToken());
-          assertEquals("0100e4feOiJSUzUxMiJ97oqGf729d1866f", symBotRSAAuth.getKmToken());
-      } catch (AuthenticationException e) {
-          fail();
-      }
-  }
+            assertNotNull(symBotRSAAuth.getSessionToken());
+            assertEquals("eyJhbGciOiJSUzUxMiJ97oqG1Kd28l1FpQ", symBotRSAAuth.getSessionToken());
+            assertNotNull(symBotRSAAuth.getKmToken());
+            assertEquals("0100e4feOiJSUzUxMiJ97oqGf729d1866f", symBotRSAAuth.getKmToken());
+        } catch (AuthenticationException e) {
+            fail();
+        }
+    }
 }
--- src/test/java/it/commons/BaseTest.java
+++ src/test/java/it/commons/BaseTest.java
@@ -4,20 +4,46 @@ import clients.SymBotClient;
 import configuration.SymConfig;
 import configuration.SymConfigLoader;
 import java.io.InputStream;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
 public class BaseTest {
-  protected static SymConfig config;
+    protected static SymConfig config;
 
-  @BeforeClass
-  public static void setUp() {
-    InputStream configFileStream = BaseTest.class.getResourceAsStream("/bot-config.json");
-    config = SymConfigLoader.load(configFileStream);
-  }
+    @BeforeClass
+    public static void setUp() {
+        InputStream configFileStream = BaseTest.class.getResourceAsStream("/bot-config.json");
+        config = SymConfigLoader.load(configFileStream);
 
-  @Before
-  public void resetSymBot() {
-    SymBotClient.clearBotClient();
-  }
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(
+                null,
+                new TrustManager[] {
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+                        public void checkClientTrusted(X509Certificate[] c, String a) {}
+                        public void checkServerTrusted(X509Certificate[] c, String a) {}
+                    }
+                },
+                new SecureRandom()
+            );
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Before
+    public void resetSymBot() {
+        SymBotClient.clearBotClient();
+    }
 }
