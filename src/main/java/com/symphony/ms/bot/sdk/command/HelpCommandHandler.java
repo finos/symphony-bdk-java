bc13f8de64cdb527465a7ad304d9d0c00b9a2e4f
Marcus Secato <44905507+marcus-secato-symphony@users.noreply.github.com>
2020-01-16 10:31:26 -0300

    Adjust help command

--- src/main/java/com/symphony/ms/bot/sdk/command/HelpCommandHandler.java
+++ src/main/java/com/symphony/ms/bot/sdk/command/HelpCommandHandler.java
@@ -25,31 +25,9 @@ public class HelpCommandHandler extends CommandHandler {
    */
   @Override
   public void handle(BotCommand command, SymphonyMessage response) {
-    String botMention = "<b>@" + getBotName() + "</b>";
-    String[] commands = {
-        botMention + " /hello - simple hello command",
-        botMention + " /help - displays the list of commands",
-        botMention
-            + " /create notification - generates details on how to receive notification in this "
-            + "room",
-        botMention
-            + " /login - returns the HTTP authorization header required to talk to external system",
-        botMention + " /quote BRL - returns quote for the specified currency (e.g. BRL)",
-        botMention + " /register quote - displays the currency quote registration form",
-        botMention + " /template alert {\"title\": \"Title\", \"content\": \"Content\"} - "
-            + "displays a customized alert"};
-
-    String helpMessage = "<p style='margin-bottom:6px;'>Bot Commands</p><ul>";
-    for (int i = 0; i < commands.length; i++) {
-      helpMessage = helpMessage.concat("<li>" + commands[i] + "</li>");
-    }
-    helpMessage = helpMessage.concat("</ul>");
-
     Map<String, Object> data = new HashMap<>();
-    data.put("title", "Bot Commands");
-    data.put("content", commands);
-
-    response.setEnrichedMessage(helpMessage, "com.symphony.ms.template.helpCommand", data, "1.0");
+    data.put("bot_mention", "@" + getBotName());
+    response.setTemplateFile("help-response", data);
   }
 
 }
