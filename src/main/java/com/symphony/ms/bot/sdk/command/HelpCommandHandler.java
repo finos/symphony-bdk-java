cbec243c17d0c8f5fe65cfcdc35e796e89830735
Marcus Secato <44905507+marcus-secato-symphony@users.noreply.github.com>
2020-01-20 08:43:40 -0300

    adjust help sample

--- src/main/java/com/symphony/ms/bot/sdk/command/HelpCommandHandler.java
+++ src/main/java/com/symphony/ms/bot/sdk/command/HelpCommandHandler.java
@@ -13,6 +13,16 @@ import com.symphony.ms.bot.sdk.internal.message.model.SymphonyMessage;
  */
 public class HelpCommandHandler extends CommandHandler {
 
+  private static final String[] DESCRIPTIONS = {
+      "/hello - simple hello command",
+      "/help - displays the list of commands",
+      "/create notification - generates details on how to receive notification in this room",
+      "/login - returns the HTTP authorization header required to talk to external system",
+      "/quote BRL - returns quote for the specified currency (e.g. BRL)",
+      "/register quote - displays the currency quote registration form",
+      "/template alert {\"title\": \"Title\", \"content\": \"Content\"} - displays a customized alert"
+  };
+
   @Override
   protected Predicate<String> getCommandMatcher() {
     return Pattern
@@ -27,6 +37,7 @@ public class HelpCommandHandler extends CommandHandler {
   public void handle(BotCommand command, SymphonyMessage response) {
     Map<String, Object> data = new HashMap<>();
     data.put("bot_mention", "@" + getBotName());
+    data.put("descriptions", DESCRIPTIONS);
     response.setTemplateFile("help-response", data);
   }
 
