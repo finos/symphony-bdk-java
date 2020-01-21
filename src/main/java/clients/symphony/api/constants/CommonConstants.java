c8db2662b3cff57f8f0823b939b612f63bb84f22
Yong Sheng Tan <yong.tan@symphony.com>
2020-01-21 14:26:05 +0800

    Added support for DLP message rejection

--- src/main/java/clients/symphony/api/constants/CommonConstants.java
+++ src/main/java/clients/symphony/api/constants/CommonConstants.java
@@ -2,4 +2,5 @@ package clients.symphony.api.constants;
 
 public class CommonConstants {
     public static final String HTTPS_PREFIX = "https://";
+    public static final int DLP_BLOCKED = 451;
 }
