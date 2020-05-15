073561d948d00d6806442005ba58ba6f4a4e25e4
Lucas <lmacedo@daitangroup.com>
2020-05-15 09:29:11 -0300

    Adding ignoreUnknown=true on RoomSearchQuery

--- src/main/java/model/RoomSearchQuery.java
+++ src/main/java/model/RoomSearchQuery.java
@@ -1,8 +1,10 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RoomSearchQuery {
     private String query;
     private List<String> labels;
