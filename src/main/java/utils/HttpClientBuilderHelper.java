dd2e383e7a098b67ad20b58d56072aeae4949c2a
Thibault Pensec <39826516+symphony-thibault@users.noreply.github.com>
2020-06-01 21:31:12 +0200

    SDK-75 Fixed random SSL issues for RSA authentication (#118)
    * SDK-75 Fixed SSL issues for RSA authentication
    
    * SDK-75 Added doc for Retry configuration
    
    * SDK-75 Added unit tests, simplified SymConfig class with Lombok usage
    
    * SDK-75 Added more unit tests, better error management in SymBotRSAAuth when JWT needs to be refreshed
    
    (cherry picked from commit 86e5410c171bbe0ea678e55b9cbc5b51d229c845)

--- src/main/java/utils/HttpClientBuilderHelper.java
+++ src/main/java/utils/HttpClientBuilderHelper.java
@@ -1,116 +1,110 @@
 package utils;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 import configuration.SymConfig;
-import java.io.*;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import javax.ws.rs.client.ClientBuilder;
+import internal.FileHelper;
+import internal.jersey.NoCacheFeature;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.glassfish.jersey.SslConfigurator;
 import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import utils.jersey.NoCacheFeature;
 
-import static org.apache.commons.lang3.StringUtils.isEmpty;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
 
+import javax.annotation.Nullable;
+import javax.net.ssl.SSLContext;
+import javax.ws.rs.client.ClientBuilder;
+
+/**
+ * Set of support methods for creating and initializing Jersey {@link javax.ws.rs.client.Client} for Pod, Agent, KM, etc.
+ */
+@Slf4j
 public class HttpClientBuilderHelper {
-    private static final Logger logger = LoggerFactory.getLogger(HttpClientBuilderHelper.class);
+
+    private static final String TRUSTSTORE_FORMAT = "JKS";
 
     public static ClientBuilder getHttpClientBuilderWithTruststore(SymConfig config) {
-        KeyStore jksStore = getJksKeystore();
-        ClientBuilder clientBuilder = ClientBuilder.newBuilder();
-
-        if (config.getTruststorePath() != null && jksStore != null) {
-            loadTrustStore(config, jksStore, clientBuilder);
-        }
-
-        clientBuilder.register(NoCacheFeature.class);
-
-        return clientBuilder;
+        return ClientBuilder.newBuilder().register(NoCacheFeature.class)
+            .sslContext(createSSLContext(
+                config.getTruststorePath(),
+                config.getTruststorePassword(),
+                null,
+                null
+            ));
     }
 
     public static ClientBuilder getHttpClientBotBuilder(SymConfig config) {
-        KeyStore pkcsStore = getPkcsKeystore();
-        KeyStore jksStore = getJksKeystore();
-
-        try (InputStream keyStoreIS = loadInputStream(config.getBotCertPath() + config.getBotCertName())) {
-            if (pkcsStore != null) {
-                pkcsStore.load(keyStoreIS, config.getBotCertPassword().toCharArray());
-            }
-        } catch (CertificateException | NoSuchAlgorithmException | IOException e) {
-            logger.error("Error loading bot keystore file", e);
-        }
-
-        ClientBuilder clientBuilder = ClientBuilder.newBuilder().keyStore(pkcsStore, config.getBotCertPassword().toCharArray());
-        if (config.getTruststorePath() != null && jksStore != null) {
-            loadTrustStore(config, jksStore, clientBuilder);
-        }
-        clientBuilder.register(NoCacheFeature.class);
-
-        return clientBuilder;
+        return ClientBuilder.newBuilder().register(NoCacheFeature.class)
+            .sslContext(createSSLContext(
+                config.getTruststorePath(),
+                config.getTruststorePassword(),
+                FileHelper.path(config.getBotCertPath(), config.getBotCertName()),
+                config.getBotCertPassword()
+            ));
     }
 
     public static ClientBuilder getHttpClientAppBuilder(SymConfig config) {
-        KeyStore pkcsStore = getPkcsKeystore();
-        KeyStore jksStore = getJksKeystore();
-
-        try (InputStream keyStoreIS = loadInputStream(config.getAppCertPath() + config.getAppCertName())) {
-            if (pkcsStore != null) {
-                pkcsStore.load(keyStoreIS, config.getBotCertPassword().toCharArray());
-            }
-        } catch (CertificateException | NoSuchAlgorithmException | IOException e) {
-            logger.error("Error loading app keystore file", e);
-        }
-
-        ClientBuilder clientBuilder = ClientBuilder.newBuilder().keyStore(pkcsStore, config.getAppCertPassword().toCharArray());
-        if (config.getTruststorePath() != null && jksStore != null) {
-            loadTrustStore(config, jksStore, clientBuilder);
-        }
-
-        clientBuilder.register(NoCacheFeature.class);
-
-        return clientBuilder;
+        return ClientBuilder.newBuilder().register(NoCacheFeature.class)
+            .sslContext(createSSLContext(
+                config.getTruststorePath(),
+                config.getTruststorePassword(),
+                FileHelper.path(config.getAppCertPath(), config.getAppCertName()),
+                config.getAppCertPassword()
+            ));
     }
 
     public static ClientConfig getPodClientConfig(SymConfig config) {
-        String proxyURL = !isEmpty(config.getPodProxyURL()) ?
-            config.getPodProxyURL() : config.getProxyURL();
-        String proxyUser = !isEmpty(config.getPodProxyUsername()) ?
-            config.getPodProxyUsername() : config.getProxyUsername();
-        String proxyPass = !isEmpty(config.getPodProxyPassword()) ?
-            config.getPodProxyPassword() : config.getProxyPassword();
-
-        return getClientConfig(config, proxyURL, proxyUser, proxyPass);
+        return getClientConfig(
+            config,
+            getOr(config.getPodProxyURL(), config.getProxyURL()),
+            getOr(config.getPodProxyUsername(), config.getProxyUsername()),
+            getOr(config.getPodProxyPassword(), config.getProxyPassword())
+        );
     }
 
     public static ClientConfig getAgentClientConfig(SymConfig config) {
-        String proxyURL = config.getProxyURL();
-        String proxyUser = config.getProxyUsername();
-        String proxyPass = config.getProxyPassword();
-
-        return getClientConfig(config, proxyURL, proxyUser, proxyPass);
+        return getClientConfig(
+            config,
+            getOr(config.getAgentProxyURL(), config.getProxyURL()),
+            getOr(config.getAgentProxyUsername(), config.getProxyUsername()),
+            getOr(config.getAgentProxyPassword(), config.getProxyPassword())
+        );
     }
 
     public static ClientConfig getKMClientConfig(SymConfig config) {
-        String kmProxyURL = config.getKeyManagerProxyURL();
-        String kmProxyUser = config.getKeyManagerProxyUsername();
-        String kmProxyPass = config.getKeyManagerProxyPassword();
-
-        return getClientConfig(config, kmProxyURL, kmProxyUser, kmProxyPass);
+        return getClientConfig(
+            config,
+            getOr(config.getKeyManagerProxyURL(), config.getProxyURL()),
+            getOr(config.getKeyManagerProxyUsername(), config.getProxyURL()),
+            getOr(config.getKeyManagerProxyPassword(), config.getProxyURL())
+        );
     }
 
-    private static ClientConfig getClientConfig(
-        SymConfig config, String proxyURL, String proxyUser, String proxyPass
-    ) {
-        ClientConfig clientConfig = new ClientConfig();
-        if (config.getConnectionTimeout() == 0) {
-            config.setConnectionTimeout(35000);
-        }
+    private static ClientConfig getClientConfig(SymConfig config, String proxyURL, String proxyUser, String proxyPass) {
+        final ClientConfig clientConfig = new ClientConfig();
+
         clientConfig.property(ClientProperties.CONNECT_TIMEOUT, config.getConnectionTimeout());
-        clientConfig.property(ClientProperties.READ_TIMEOUT, config.getConnectionTimeout());
+        clientConfig.property(ClientProperties.READ_TIMEOUT, config.getReadTimeout());
 
         if (!isEmpty(proxyURL)) {
             clientConfig.connectorProvider(new ApacheConnectorProvider());
@@ -121,45 +115,44 @@ public class HttpClientBuilderHelper {
             }
         }
 
-        clientConfig.register(NoCacheFeature.class);
-
         return clientConfig;
     }
 
-    private static void loadTrustStore(SymConfig config, KeyStore tks, ClientBuilder clientBuilder) {
-        try (InputStream trustStoreIS = loadInputStream(config.getTruststorePath())) {
-            tks.load(trustStoreIS, config.getTruststorePassword().toCharArray());
-            clientBuilder.trustStore(tks);
-        } catch (CertificateException | NoSuchAlgorithmException | IOException e) {
-            logger.error("Error loading truststore", e);
+    @SneakyThrows
+    private static SSLContext createSSLContext(
+        @Nullable final String truststorePath,
+        @Nullable final String truststorePassword,
+        @Nullable final String keystorePath,
+        @Nullable final String keystorePassword
+    ) {
+        final SslConfigurator sslConfig = SslConfigurator.newInstance();
+
+        if (!isEmpty(truststorePath) && !isEmpty(truststorePassword)) {
+            final byte[] trustStoreBytes = FileHelper.readFile(truststorePath);
+            final KeyStore truststore = KeyStore.getInstance(TRUSTSTORE_FORMAT);
+            truststore.load(new ByteArrayInputStream(trustStoreBytes), truststorePassword.toCharArray());
+            // if logging debug is enabled, we print the truststore entries
+            if(logger.isDebugEnabled()) {
+                final List<String> aliases = Collections.list(truststore.aliases());
+                logger.debug("Your custom truststore ('{}') contains {} entries :", truststorePath, aliases.size());
+                for (String alias : aliases) {
+                    logger.debug("# {}", alias);
+                }
+            }
+            sslConfig.trustStore(truststore);
         }
+
+        if (!isEmpty(keystorePath) && !isEmpty(keystorePassword)) {
+            final byte[] keystoreBytes = FileHelper.readFile(keystorePath);
+            sslConfig
+                .keyStoreBytes(keystoreBytes)
+                .keyStorePassword(keystorePassword);
+        }
+
+        return sslConfig.createSSLContext();
     }
 
-    private static InputStream loadInputStream(String fileName) throws FileNotFoundException {
-        if ((new File(fileName)).exists()) {
-            return new FileInputStream(fileName);
-        } else if (HttpClientBuilderHelper.class.getResource(fileName) != null) {
-            return HttpClientBuilderHelper.class.getResourceAsStream(fileName);
-        } else {
-            throw new FileNotFoundException();
-        }
-    }
-
-    private static KeyStore getPkcsKeystore() {
-        try {
-            return KeyStore.getInstance("PKCS12");
-        } catch (KeyStoreException e) {
-            logger.error("Error creating PKCS keystore instance", e);
-        }
-        return null;
-    }
-
-    private static KeyStore getJksKeystore() {
-        try {
-            return KeyStore.getInstance("JKS");
-        } catch (KeyStoreException e) {
-            logger.error("Error creating JKS keystore instance", e);
-        }
-        return null;
-    }
+  private static String getOr(final String preferredValue, final String fallbackValue) {
+    return !isEmpty(preferredValue) ? preferredValue : fallbackValue;
+  }
 }
