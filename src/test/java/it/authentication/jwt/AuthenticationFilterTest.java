6f91b814b3e26ac3fcae636cc89f23248f99d669
symphony-hong <hong.le@symphony.com>
2020-06-05 17:28:16 +0200

    SDK-81: Update unittest

--- src/test/java/it/authentication/jwt/AuthenticationFilterTest.java
+++ src/test/java/it/authentication/jwt/AuthenticationFilterTest.java
@@ -3,7 +3,7 @@ package it.authentication.jwt;
 import authentication.AuthEndpointConstants;
 import authentication.SymExtensionAppRSAAuth;
 import authentication.jwt.AuthenticationFilter;
-import it.commons.ServerTest;
+import it.commons.BotTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,49 +14,33 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class AuthenticationFilterTest extends ServerTest {
+public class AuthenticationFilterTest extends BotTest {
 
     private static AuthenticationFilter authFilter;
     private static final String MISSING_JWT_MESSAGE = "Missing JWT";
     private static final String UNAUTHORIZED_JWT_MESSAGE = "Unauthorized JWT";
 
     @Mock
-    private static HttpServletRequest mRequest;
+    private HttpServletRequest mRequest;
     @Mock
-    private static HttpServletResponse mResponse;
+    private HttpServletResponse mResponse;
     @Mock
-    private static FilterChain mFilterChain;
+    private FilterChain mFilterChain;
     @Mock
-    private static PrintWriter mWriter;
+    private PrintWriter mWriter;
 
     @Before
     public void setup() {
         config.setAuthenticationFilterUrlPattern("");
-        stubFor(get(urlEqualTo(AuthEndpointConstants.POD_CERT_RSA_PATH))
-                .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-                        .withBody("{\"certificate\":\"-----BEGIN CERTIFICATE-----\\nMIIEQDCCAyigAwIBAgIVAKmSDvvk3rea1n\\n-----END CERTIFICATE-----\\n\"}")));
-
-        stubFor(post(urlEqualTo(AuthEndpointConstants.SESSION_EXT_APP_AUTH_PATH_RSA))
-                .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-                        .withBody("{ \"appId\" : \"APP_ID\", \"appToken\" : \"APP_TOKEN\", \"symphonyToken\" : \"SYMPHONY_TOKEN\", \"expireAt\" : 1539636528288 }")));
-
+        stubGet(AuthEndpointConstants.POD_CERT_RSA_PATH, "{\"certificate\":\"-----BEGIN CERTIFICATE-----\\nMIIEQDCCAyigAwIBAgIVAKmSDvvk3rea1n\\n-----END CERTIFICATE-----\\n\"}");
+        stubPost(AuthEndpointConstants.SESSION_EXT_APP_AUTH_PATH_RSA, "{ \"appId\" : \"APP_ID\", \"appToken\" : \"APP_TOKEN\", \"symphonyToken\" : \"SYMPHONY_TOKEN\", \"expireAt\" : 1539636528288 }");
         authFilter = new AuthenticationFilter(new SymExtensionAppRSAAuth(config), config);
     }
 
