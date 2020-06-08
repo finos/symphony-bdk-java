6851626f58ee75ffc67b75e6e0906293798f04c8
symphony-hong <65538951+symphony-hong@users.noreply.github.com>
2020-06-08 12:08:56 +0200

    1.1 rc (#123)
    * SDK-80 Changing caching strategy of SymOBOClient (#120)
    
    * SDK-75: Log the alias of all entries in trustore when init bot
    
    * SDK-80 Changing caching strategy of SymOBOClient
    
    * SDK-80 Changing caching strategy of SymOBOClient
    
    * Remove unused imports
    
    (cherry picked from commit 199e9e4cf0cbeb30acb67af8c0e13b25c8384497)
    
    * SDK-81 Handle null authorization header (#122)
    
    * SDK-81 Handle null authorization header
    
    * SDK-81: Update unittest
    
    (cherry picked from commit 0d14b57a1d098a887601ddc179abe38c5e646428)

--- src/test/java/it/clients/SymBotOBOClientTest.java
+++ src/test/java/it/clients/SymBotOBOClientTest.java
@@ -1,15 +1,36 @@
 package it.clients;
 
 import authentication.SymBotRSAAuth;
+import authentication.SymOBOUserRSAAuth;
 import clients.SymOBOClient;
 import it.commons.BaseTest;
 import org.junit.Test;
-import static org.junit.Assert.assertNotNull;
+
+import static org.junit.Assert.*;
 
 public class SymBotOBOClientTest extends BaseTest {
   @Test
   public void initOBOClientSuccess() {
     SymOBOClient symOBOClient = SymOBOClient.initOBOClient(config, new SymBotRSAAuth(config));
     assertNotNull(symOBOClient);
+  }
+
+  @Test
+  public void initOBOClientWithSameUserAuth() {
+    SymOBOUserRSAAuth userAuth = new SymOBOUserRSAAuth(config, null,"username", null);
+    SymOBOClient symOBOClient1 = SymOBOClient.initOBOClient(config, userAuth);
+    SymOBOClient symOBOClient2 = SymOBOClient.initOBOClient(config, userAuth);
+    assertEquals(symOBOClient1, symOBOClient2);
+  }
+
+  @Test
+  public void initOBOClientWithDiffUserAuth() {
+    SymOBOUserRSAAuth userAuth = new SymOBOUserRSAAuth(config, null, "username-1", null);
+    SymOBOClient symOBOClient1 = SymOBOClient.initOBOClient(config, userAuth);
+    SymOBOClient symOBOClient2 = SymOBOClient.initOBOClient(config,
+            new SymOBOUserRSAAuth(config, null, "username-2", null));
+    SymOBOClient symOBOClient3 = SymOBOClient.initOBOClient(config, userAuth);
+    assertNotEquals(symOBOClient1, symOBOClient2);
+    assertEquals(symOBOClient1, symOBOClient3);
   }
 }
