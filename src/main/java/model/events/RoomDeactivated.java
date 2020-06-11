4b740eab079078cc1122d5ab9180fcd008120ba6
symphony-hong <65538951+symphony-hong@users.noreply.github.com>
2020-06-11 16:45:21 +0200

    SDK-59 Add Initiator to RoomListener callback (#126)
    * SDK-59 Add Initiator to RoomListener callback
    
    * SDK-59 Add Initiator to RoomListener callbacks
    
    * Remove unused import

--- src/main/java/model/events/RoomDeactivated.java
+++ src/main/java/model/events/RoomDeactivated.java
@@ -1,11 +1,21 @@
 package model.events;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import model.Initiator;
 import model.Stream;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RoomDeactivated {
     private Stream stream;
+    private Initiator initiator;
+
+    public Initiator getInitiator() {
+        return initiator;
+    }
+
+    public void setInitiator(Initiator initiator) {
+        this.initiator = initiator;
+    }
 
     public Stream getStream() {
         return stream;
