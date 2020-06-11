a0e907c3da68c9109427b33f0498ba84d9aeb7c0
Elias Croze <66254040+symphony-elias@users.noreply.github.com>
2020-06-11 09:58:17 +0200

    Added unittest for SDK-63: check unknown fields returned by healtcheck api are ignored (#127)

--- src/test/java/it/clients/symphony/api/HealthcheckClientTest.java
+++ src/test/java/it/clients/symphony/api/HealthcheckClientTest.java
@@ -3,11 +3,14 @@ package it.clients.symphony.api;
 import clients.symphony.api.HealthcheckClient;
 import clients.symphony.api.constants.AgentConstants;
 import it.commons.BotTest;
+
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+
 import model.HealthcheckResponse;
 import org.junit.Before;
 import org.junit.Test;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -22,20 +25,15 @@ public class HealthcheckClientTest extends BotTest {
 
   @Test
   public void performHealthCheckSuccess() {
-    stubFor(get(urlEqualTo(AgentConstants.HEALTHCHECK))
-        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-        .willReturn(aResponse()
-            .withStatus(200)
-            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-            .withBody("{\r\n" +
-                "\"podConnectivity\": true,\r\n" +
-                "\"keyManagerConnectivity\": true,\r\n" +
-                "\"encryptDecryptSuccess\": true,\r\n" +
-                "\"podVersion\": \"1.54.1\",\r\n" +
-                "\"agentVersion\": \"2.54.0\",\r\n" +
-                "\"agentServiceUser\": true,\r\n" +
-                "\"ceServiceUser\": true\r\n" +
-                "}")));
+    stubGet(AgentConstants.HEALTHCHECK, "{\r\n" +
+        "\"podConnectivity\": true,\r\n" +
+        "\"keyManagerConnectivity\": true,\r\n" +
+        "\"encryptDecryptSuccess\": true,\r\n" +
+        "\"podVersion\": \"1.54.1\",\r\n" +
+        "\"agentVersion\": \"2.54.0\",\r\n" +
+        "\"agentServiceUser\": true,\r\n" +
+        "\"ceServiceUser\": true\r\n" +
+        "}");
 
     HealthcheckResponse response = healthCheckClient.performHealthCheck();
 
@@ -43,4 +41,22 @@ public class HealthcheckClientTest extends BotTest {
     assertEquals(true, response.getPodConnectivity());
   }
 
+  @Test
+  public void performHealthCheckSuccessWithUnknownField() {
+    stubGet(AgentConstants.HEALTHCHECK, "{\r\n" +
+        "\"podConnectivity\": true,\r\n" +
+        "\"keyManagerConnectivity\": true,\r\n" +
+        "\"encryptDecryptSuccess\": true,\r\n" +
+        "\"podVersion\": \"1.54.1\",\r\n" +
+        "\"agentVersion\": \"2.54.0\",\r\n" +
+        "\"agentServiceUser\": true,\r\n" +
+        "\"ceServiceUser\": true\r\n," +
+        "\"unknownField\": \"a value\"\r\n" +
+        "}");
+
+    HealthcheckResponse response = healthCheckClient.performHealthCheck();
+
+    assertNotNull(response);
+    assertEquals(true, response.getPodConnectivity());
+  }
 }
