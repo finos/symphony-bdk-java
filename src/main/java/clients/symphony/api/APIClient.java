4f29261f684219490da6ed208fb3b4cb8f8bce3e
symphony-hong <hong.le@symphony.com>
2020-06-18 11:28:33 +0200

    Handle response content not-json formatted

--- src/main/java/clients/symphony/api/APIClient.java
+++ src/main/java/clients/symphony/api/APIClient.java
@@ -14,6 +14,17 @@ public abstract class APIClient {
     private final Logger logger = LoggerFactory.getLogger(APIClient.class);
     private static final ObjectMapper mapper = new ObjectMapper();
 
+    private ClientError readClientError(Response response) {
+        String errorString = response.readEntity(String.class);
+        try {
+            return mapper.readValue(errorString, ClientError.class);
+        } catch (JsonProcessingException e) {
+            ClientError error = new ClientError();
+            error.setMessage(errorString);
+            return error;
+        }
+    }
+
     protected void handleError(Response response, ISymClient botClient) throws SymClientException {
         if (response.getStatusInfo().getFamily() == Family.SERVER_ERROR) {
             logger.error("REST error: error code {} reason {}",
@@ -22,16 +33,9 @@ public abstract class APIClient {
             );
             throw new ServerErrorException(response.getStatusInfo().getReasonPhrase());
         } else {
-            String errorString = response.readEntity(String.class);
-            ClientError error;
-            try {
-                error = mapper.readValue(errorString, ClientError.class);
-            } catch (JsonProcessingException e) {
-                throw new APIClientErrorException(e.getMessage());
-            }
-
+            ClientError error = this.readClientError(response);
             if (response.getStatus() == BAD_REQUEST.getStatusCode()) {
-                logger.error("Client error occurred: {}", error);
+                logger.error("Client error occurred: {}", error.getMessage());
                 throw new APIClientErrorException(error.getMessage());
             } else if (response.getStatus() == UNAUTHORIZED.getStatusCode()) {
                 logger.error("User unauthorized, refreshing tokens");
