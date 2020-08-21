78fd6c1caf76b703641a8d644cb557e44d80edf2
symphony-hong <hong.le@symphony.com>
2020-08-21 10:27:07 +0200

    Update the datafeed service example
    Simply start datafeed service in main thread as a blocking call,
    like starting a server in Java

--- symphony-bdk-examples/bdk-core-examples/src/main/java/com/symphony/bdk/examples/DatafeedExampleMain.java
+++ symphony-bdk-examples/bdk-core-examples/src/main/java/com/symphony/bdk/examples/DatafeedExampleMain.java
@@ -7,16 +7,10 @@ import com.symphony.bdk.core.auth.exception.AuthUnauthorizedException;
 import com.symphony.bdk.core.config.BdkConfigLoader;
 import com.symphony.bdk.core.config.exception.BdkConfigException;
 import com.symphony.bdk.core.config.model.BdkConfig;
-import com.symphony.bdk.core.service.BotInfoService;
 import com.symphony.bdk.core.service.datafeed.DatafeedEventListener;
 import com.symphony.bdk.core.service.datafeed.DatafeedService;
-import com.symphony.bdk.gen.api.model.UserV2;
 import com.symphony.bdk.gen.api.model.V4Event;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class DatafeedExampleMain {
@@ -27,39 +21,22 @@ public class DatafeedExampleMain {
         final BdkConfig config = BdkConfigLoader.loadFromClasspath("/config.yaml");
 
         SymphonyBdk bdk = new SymphonyBdk(config);
-
-        log.info("Config: " + config.getPod().getBasePath());
-        BotInfoService infoService = bdk.botInfo();
-        UserV2 userInfo = infoService.getBotInfo();
-        log.info("UserId: " + userInfo.getId());
-        log.info("Second time: " + bdk.botInfo().getBotInfo().getCompany());
-
         log.info("DatafeedV1: Start");
         DatafeedService ds = bdk.datafeed();
 
         ds.subscribe(new DatafeedEventListener() {
             @Override
             public void onMessageSent(V4Event event) {
-                log.info("Message Sent");
+                if (event.getPayload() != null && event.getPayload().getMessageSent() != null
+                        && event.getPayload().getMessageSent().getMessage() != null) {
+                    log.info("Message with id {} successfully received !", event.getPayload().getMessageSent().getMessage().getMessageId());
+                }
             }
         });
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.submit(() -> {
-            try {
-                ds.start();
-            } catch (Throwable throwable) {
-                throwable.printStackTrace();
-            }
-        });
-        Thread.sleep(10000);
-        ds.stop();
-        executorService.shutdown();
         try {
-            if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
-                executorService.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            executorService.shutdownNow();
+            ds.start();
+        } finally {
+            ds.stop();
         }
     }
 }
