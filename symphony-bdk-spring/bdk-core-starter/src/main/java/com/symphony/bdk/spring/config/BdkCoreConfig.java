a296d6977444e7bbbe7871215c7e9ee76b441064
symphony-thibault <thibault.pensec@symphony.com>
2020-08-26 21:12:05 +0200

    APP-2814 Removed OboAuthentication from injection (failure when app not configured), simplified application.yaml

--- symphony-bdk-spring/bdk-core-starter/src/main/java/com/symphony/bdk/spring/config/BdkCoreConfig.java
+++ symphony-bdk-spring/bdk-core-starter/src/main/java/com/symphony/bdk/spring/config/BdkCoreConfig.java
@@ -3,12 +3,12 @@ package com.symphony.bdk.spring.config;
 import com.symphony.bdk.core.api.invoker.jersey2.ApiClientProviderJersey2;
 import com.symphony.bdk.core.auth.AuthSession;
 import com.symphony.bdk.core.auth.AuthenticatorFactory;
-import com.symphony.bdk.core.auth.OboAuthenticator;
 import com.symphony.bdk.core.auth.exception.AuthInitializationException;
 import com.symphony.bdk.core.auth.exception.AuthUnauthorizedException;
 import com.symphony.bdk.core.client.ApiClientFactory;
 import com.symphony.bdk.spring.SymphonyBdkCoreProperties;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.Primary;
 /**
  * Configuration and injection of the main BDK/Core classes as beans within the Spring application context.
  */
+@Slf4j
 public class BdkCoreConfig {
 
   @Primary
@@ -41,16 +42,6 @@ public class BdkCoreConfig {
       return authenticatorFactory.getBotAuthenticator().authenticateBot();
     } catch (AuthUnauthorizedException | AuthInitializationException e) {
       throw new BeanInitializationException("Unable to authenticate bot", e);
-    }
-  }
-
-  @Primary
-  @Bean(name = "defaultSymphonyBdkOboAuthenticator")
-  public OboAuthenticator oboAuthenticator(AuthenticatorFactory authenticatorFactory) {
-    try {
-      return authenticatorFactory.getOboAuthenticator();
-    } catch (AuthInitializationException e) {
-      throw new BeanInitializationException("Unable to create OBO authenticator", e);
     }
   }
 }
