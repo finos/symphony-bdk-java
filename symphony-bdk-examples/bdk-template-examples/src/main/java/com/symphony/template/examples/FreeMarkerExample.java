fdfb4733c2d8b253748bc81f2e348863c58f3fa4
Elias Croze <elias.croze@symphony.com>
2020-09-04 17:11:50 +0200

    Updated template examples

--- symphony-bdk-examples/bdk-template-examples/src/main/java/com/symphony/template/examples/FreeMarkerExample.java
+++ symphony-bdk-examples/bdk-template-examples/src/main/java/com/symphony/template/examples/FreeMarkerExample.java
@@ -1,22 +1,25 @@
 package com.symphony.template.examples;
 
 import com.symphony.bdk.template.api.Template;
+import com.symphony.bdk.template.api.TemplateEngine;
 import com.symphony.bdk.template.api.TemplateException;
-import com.symphony.bdk.template.freemarker.FreeMarkerEngine;
+
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 
 /**
  * Sample class showing usage of FreeMarker templates.
  */
+@Slf4j
 public class FreeMarkerExample {
   public static void main(String[] args) throws TemplateException {
-    System.out.println(getStringFromBuiltInTemplate());
-    System.out.println(getStringFromCustomTemplate());
+    log.info("String generated from built-in template: {}", getStringFromBuiltInTemplate());
+    log.info("String generated from custom template: {}", getStringFromCustomTemplate());
   }
 
   private static String getStringFromBuiltInTemplate() throws TemplateException {
-    Template template = new FreeMarkerEngine().newBuiltInTemplate("simpleMML");
+    Template template = TemplateEngine.getDefaultImplementation().newBuiltInTemplate("simpleMML");
     final String message = template.process(new HashMap<String, String>() {{
       put("message", "Hello World !");
     }});
@@ -24,7 +27,8 @@ public class FreeMarkerExample {
   }
 
   private static String getStringFromCustomTemplate() throws TemplateException {
-    Template template = new FreeMarkerEngine().newTemplateFromClasspath("templates/customTemplate.ftl");
+    Template template = TemplateEngine.getDefaultImplementation()
+        .newTemplateFromClasspath("templates/customTemplate.ftl");
     final String message = template.process(new HashMap<String, String>() {{
       put("message", "Hello World");
     }});
