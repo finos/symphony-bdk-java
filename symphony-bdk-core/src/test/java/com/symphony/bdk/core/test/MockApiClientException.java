e06ab03501ef352928d8ed5220c11b8f1524ffd4
symphony-hong <65538951+symphony-hong@users.noreply.github.com>
2020-09-11 18:52:30 +0200

    Enhancement for MockApiClient (#210)

---
+++ symphony-bdk-core/src/test/java/com/symphony/bdk/core/test/MockApiClientException.java
@@ -0,0 +1,8 @@
+package com.symphony.bdk.core.test;
+
+public class MockApiClientException extends RuntimeException {
+
+  public MockApiClientException(String message) {
+    super(message);
+  }
+}
