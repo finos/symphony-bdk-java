cdc48abc3fe4f26851f3b994b9f1d1e17d014353
Elias Croze <66254040+symphony-elias@users.noreply.github.com>
2020-09-11 10:24:07 +0200

    APP-2981: Added messages endpoints in MessageService (#200)
    * APP-2981: Implemented remaining endpoints and integrated template API in  MessageService
    
    * Got java version at runtime to build User-Agent
    
    * Integrated pagination in MessageService

--- symphony-bdk-spring/bdk-core-starter/src/main/java/com/symphony/bdk/spring/config/BdkApiClientsConfig.java
+++ symphony-bdk-spring/bdk-core-starter/src/main/java/com/symphony/bdk/spring/config/BdkApiClientsConfig.java
@@ -15,6 +15,7 @@ import com.symphony.bdk.gen.api.DisclaimerApi;
 import com.symphony.bdk.gen.api.DlpPoliciesAndDictionaryManagementApi;
 import com.symphony.bdk.gen.api.InfoBarriersApi;
 import com.symphony.bdk.gen.api.MessageApi;
+import com.symphony.bdk.gen.api.MessageSuppressionApi;
 import com.symphony.bdk.gen.api.MessagesApi;
 import com.symphony.bdk.gen.api.PodApi;
 import com.symphony.bdk.gen.api.PresenceApi;
@@ -122,7 +123,12 @@ public class BdkApiClientsConfig {
 
   @Bean
   public MessageApi messageApi() {
-    return new MessageApi(agentApiClient());
+    return new MessageApi(podApiClient());
+  }
+
+  @Bean
+  public MessageSuppressionApi messageSuppressionApi() {
+    return new MessageSuppressionApi(podApiClient());
   }
 
   @Bean
