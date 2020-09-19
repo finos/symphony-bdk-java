11f21cd26bce10d06b9d922a8b8ab1b18edda1fc
Youri Bonnaffe <youri.bonnaffe@symphony.com>
2020-09-19 11:22:59 +0200

    Build BDK using Gradle
    Use Gradle instead of Maven for faster builds thanks to incremental
    support!
    
    Most of the configuration is at the root level and applied to all
    subprojects. Sub projects mostly define their dependencies.
    
    Use the Java platform support to implement Maven BOM and align
    dependencies.
    Gradle wrapper can be used to build.
    Circle CI configuration has been updated.
    
    Jenkins configuration for release is not yet migrated.
    Code coverage configuration for legacy BDK felt off, we need to review
    it together.

---
+++ symphony-bdk-core-invokers/symphony-bdk-core-invoker-api/src/main/java/com/symphony/bdk/core/api/invoker/JSON.java
@@ -0,0 +1,10 @@
+package com.symphony.bdk.core.api.invoker;
+
+import java.util.Map;
+
+public class JSON {
+  public static void registerDiscriminator(Class<?> messageClass, String messageType,
+      Map<String, Class<?>> mappings) {
+
+  }
+}
