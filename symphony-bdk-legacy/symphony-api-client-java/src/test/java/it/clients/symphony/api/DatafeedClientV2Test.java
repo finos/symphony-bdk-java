becd67ee1dc0c825d72b1b09b927e3e43faff786
haiddi <hserraye@avencod.fr>
2020-09-21 16:39:54 +0200

    SDK-136 Remove @Ignore annotation from class DatafeedClientV2Test

--- symphony-bdk-legacy/symphony-api-client-java/src/test/java/it/clients/symphony/api/DatafeedClientV2Test.java
+++ symphony-bdk-legacy/symphony-api-client-java/src/test/java/it/clients/symphony/api/DatafeedClientV2Test.java
@@ -31,7 +31,10 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NoContentException;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -41,21 +44,21 @@ import static org.junit.Assert.*;
 
 public class DatafeedClientV2Test extends BotTest {
 
-    private  static  final Logger logger = LoggerFactory.getLogger(DatafeedClientV2Test.class);
-    private DatafeedClient datafeedClient;
+  private  static  final Logger logger = LoggerFactory.getLogger(DatafeedClientV2Test.class);
+  private DatafeedClient datafeedClient;
 
-    @Before
-    public void initClient() throws IOException {
-        stubGet(PodConstants.GETSESSIONUSER,
-                readResourceContent("/response_content/authenticate/get_session_user.json"));
-        config.setDatafeedVersion("v2");
-        SymBotRSAAuth auth = new SymBotRSAAuth(config);
-        auth.setSessionToken("eyJhbGciOiJSUzUxMiJ9.eyJzdWIiOiJ0ZXN0LWJvdCIsImlzcyI6InN5bXBob255Iiwic2Vzc2lvbklkIjoiZmRiOTAxMmQzOTgwMGE3NzNkMTJjYWFmZGY5MjU4ZjZjOWEyMTE2MmYyZDU1ODQ3M2Y5ZDU5MDUyNjA0Mjg1ZjU0MWM5Yzg0Mzc5YTE0MjZmODNiZmZkZTljYmQ5NjRjMDAwMDAxNmRmMjMyODIwNTAwMDEzZmYwMDAwMDAxZTgiLCJ1c2VySWQiOiIzNTE3NzUwMDE0MTIwNzIifQ.DlQ_-sAqZLlAcVTr7t_PaYt_Muq_P82yYrtbEEZWMpHMl-7qCciwfi3uXns7oRbc1uvOrhQd603VKQJzQxaZBZBVlUPS-2ysH0tBpCS57ocTS6ZwtQwPLCZYdT-EZ70EzQ95kG6P5TrLENH6UveohgeDdmyzSPOEiwyEUjjmzaXFE8Tu0R3xQDwl-BKbsyUAAgd1X7T0cUDC3WIDl9xaTvyxavep4ZJnZJl4qPc1Tan0yU7JrxtXeD8uwNYlKLudT3UVxduFPMQP_2jyj5Laa-YWGKvRtXkcy2d3hzf4ll1l1wVnyJc1e6hW2EnRlff_Nxge-QCJMcZ_ALrpOUtAyQ");
-        symBotClient = SymBotClient.initBot(config, auth);
-        datafeedClient = symBotClient.getDatafeedClient();
-    }
+  @Before
+  public void initClient() throws IOException {
+    stubGet(PodConstants.GETSESSIONUSER,
+        readResourceContent("/response_content/authenticate/get_session_user.json"));
+    config.setDatafeedVersion("v2");
+    SymBotRSAAuth auth = new SymBotRSAAuth(config);
+    auth.setSessionToken("eyJhbGciOiJSUzUxMiJ9.eyJzdWIiOiJ0ZXN0LWJvdCIsImlzcyI6InN5bXBob255Iiwic2Vzc2lvbklkIjoiZmRiOTAxMmQzOTgwMGE3NzNkMTJjYWFmZGY5MjU4ZjZjOWEyMTE2MmYyZDU1ODQ3M2Y5ZDU5MDUyNjA0Mjg1ZjU0MWM5Yzg0Mzc5YTE0MjZmODNiZmZkZTljYmQ5NjRjMDAwMDAxNmRmMjMyODIwNTAwMDEzZmYwMDAwMDAxZTgiLCJ1c2VySWQiOiIzNTE3NzUwMDE0MTIwNzIifQ.DlQ_-sAqZLlAcVTr7t_PaYt_Muq_P82yYrtbEEZWMpHMl-7qCciwfi3uXns7oRbc1uvOrhQd603VKQJzQxaZBZBVlUPS-2ysH0tBpCS57ocTS6ZwtQwPLCZYdT-EZ70EzQ95kG6P5TrLENH6UveohgeDdmyzSPOEiwyEUjjmzaXFE8Tu0R3xQDwl-BKbsyUAAgd1X7T0cUDC3WIDl9xaTvyxavep4ZJnZJl4qPc1Tan0yU7JrxtXeD8uwNYlKLudT3UVxduFPMQP_2jyj5Laa-YWGKvRtXkcy2d3hzf4ll1l1wVnyJc1e6hW2EnRlff_Nxge-QCJMcZ_ALrpOUtAyQ");
+    symBotClient = SymBotClient.initBot(config, auth);
+    datafeedClient = symBotClient.getDatafeedClient();
+  }
 
-    // createDatafeed
+  // createDatafeed
   @Test
   public void createDatafeedSuccess201() throws IOException {
     stubPost(AgentConstants.CREATEDATAFEEDV2.replace("{id}", "8e7c8672220723985e8c42485edft85eg9ef7"),
@@ -171,58 +174,10 @@ public class DatafeedClientV2Test extends BotTest {
     }
   }
 
-  @Ignore
   @Test
-  public void readDatafeedV2Success(){
-//    stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "21449143d35a86461e254d28697214b4_f"),
-//        readResourceContent("/response_content/datafeedv2/read_datafeedv2.json"), 200);
-    stubFor(post(urlEqualTo(AgentConstants.READDATAFEEDV2.replace("{id}", "1")))
-        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-        .willReturn(aResponse()
-            .withStatus(200)
-            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-            .withBody("{"
-                + "[\n"
-                + "    {\n"
-                + "        \"id\": \"ulPr8a:eFFDL7\",\n"
-                + "        \"messageId\": \"CszQa6uPAA9V\",\n"
-                + "        \"timestamp\": 1536346282592,\n"
-                + "        \"type\": \"MESSAGESENT\",\n"
-                + "        \"initiator\": {\n"
-                + "            \"user\": {\n"
-                + "                \"userId\": 1456852,\n"
-                + "                \"displayName\": \"Local Bot01\",\n"
-                + "                \"email\": \"bot.user1@test.com\",\n"
-                + "                \"username\": \"bot.user1\"\n"
-                + "            }\n"
-                + "        },\n"
-                + "        \"payload\": {\n"
-                + "            \"messageSent\": {\n"
-                + "                \"message\": {\n"
-                + "                    \"messageId\": \"CszQa6uPAA9\",\n"
-                + "                    \"timestamp\": 1536346282592,\n"
-                + "                    \"message\": \"<div data-format=\\\"PresentationML\\\" data-version=\\\"2.0\\\">Hello World</div>\",\n"
-                + "                    \"data\": \"{\\\"entityIdentifier\\\":{\\\"type\\\":\\\"org.symphonyoss.fin.security\\\",\\\"version\\\":\\\"0.1\\\",\\\"id\\\":[{\\\"type\\\":\\\"org.symphonyoss.fin.security.id.isin\\\",\\\"value\\\":\\\"US0378\\\"},{\\\"type\\\":\\\"org.symphonyoss.fin.security.id.cusip\\\",\\\"value\\\":\\\"037\\\"},{\\\"type\\\":\\\"org.symphonyoss.fin.security.id.openfigi\\\",\\\"value\\\":\\\"BBG000\\\"}]}}\",\n"
-                + "                    \"user\": {\n"
-                + "                        \"userId\": 14568529,\n"
-                + "                        \"displayName\": \"Local Bot01\",\n"
-                + "                        \"email\": \"bot.user1@ntest.com\",\n"
-                + "                        \"username\": \"bot.user1\"\n"
-                + "                    },\n"
-                + "                    \"stream\": {\n"
-                + "                        \"streamId\": \"wTmSDJSNPXgB\",\n"
-                + "                        \"streamType\": \"ROOM\"\n"
-                + "                    },\n"
-                + "                    \"externalRecipients\": false,\n"
-                + "                    \"userAgent\": \"Agent-2.2.8-Linux-4.9.77-31.58.amzn1.x86_64\",\n"
-                + "                    \"originalFormat\": \"com.symphony.messageml.v2\",\n"
-                + "                    \"sid\": \"a4d08d18-0729-4b54-9c4568da\"\n"
-                + "                }\n"
-                + "            }\n"
-                + "        }\n"
-                + "    }\n"
-                + "]\n"
-                + "}")));
+  public void readDatafeedV2Success() throws IOException{
+    stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "1"),
+        readResourceContent("/response_content/datafeedv2/read_datafeedv2.json"), 200);
 
     try {
 
@@ -283,57 +238,10 @@ public class DatafeedClientV2Test extends BotTest {
     }
   }
 
-  @Ignore
   @Test
-  public void readDatafeedV5Success() {
-//    stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "21449143d35a86461e254d28697214b4_f"),
-//        readResourceContent("/response_content/datafeedv2/read_datafeedv5.json"), 200);
-
-    stubFor(post(urlEqualTo(AgentConstants.READDATAFEEDV2.replace("{id}", "1")))
-        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-        .willReturn(aResponse()
-            .withStatus(200)
-            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-            .withBody("{"
-                + "[\n"
-                + "    {\n"
-                + "        \"id\": \"eventId\",\n"
-                + "        \"timestamp\": 1536346282592,\n"
-                + "        \"type\": \"MESSAGESENT\",\n"
-                + "        \"initiator\": {\n"
-                + "            \"user\": {\n"
-                + "                \"userId\": 1456852,\n"
-                + "                \"displayName\": \"User 1\",\n"
-                + "                \"email\": \"user1@test.com\",\n"
-                + "                \"username\": \"user1\"\n"
-                + "            }\n"
-                + "        },\n"
-                + "        \"payload\": {\n"
-                + "            \"messageSent\": {\n"
-                + "                \"message\": {\n"
-                + "                    \"messageId\": \"CszQa6uPAA9\",\n"
-                + "                    \"timestamp\": 1536346282592,\n"
-                + "                    \"message\": \"<div data-format=\\\"PresentationML\\\" data-version=\\\"2.0\\\">Hello World</div>\",\n"
-                + "                    \"data\": \"{\\\"entityIdentifier\\\":{\\\"type\\\":\\\"org.symphonyoss.fin.security\\\",\\\"version\\\":\\\"0.1\\\",\\\"id\\\":[{\\\"type\\\":\\\"org.symphonyoss.fin.security.id.isin\\\",\\\"value\\\":\\\"US0378\\\"},{\\\"type\\\":\\\"org.symphonyoss.fin.security.id.cusip\\\",\\\"value\\\":\\\"037\\\"},{\\\"type\\\":\\\"org.symphonyoss.fin.security.id.openfigi\\\",\\\"value\\\":\\\"BBG000\\\"}]}}\",\n"
-                + "                    \"user\": {\n"
-                + "                        \"userId\": 1456852,\n"
-                + "                        \"displayName\": \"User 1\",\n"
-                + "                        \"email\": \"user1@ntest.com\",\n"
-                + "                        \"username\": \"user1\"\n"
-                + "                    },\n"
-                + "                    \"stream\": {\n"
-                + "                        \"streamId\": \"wTmSDJSNPXgB\",\n"
-                + "                        \"streamType\": \"ROOM\"\n"
-                + "                    },\n"
-                + "                    \"externalRecipients\": false,\n"
-                + "                    \"userAgent\": \"Agent-2.2.8-Linux-4.9.77-31.58.amzn1.x86_64\",\n"
-                + "                    \"originalFormat\": \"com.symphony.messageml.v2\"\n"
-                + "                }\n"
-                + "            }\n"
-                + "        }\n"
-                + "    }\n"
-                + "]\n"
-                + "}")));
+  public void readDatafeedV5Success() throws IOException {
+    stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "1"),
+        readResourceContent("/response_content/datafeedv2/read_datafeedv5.json"), 200);
 
     try {
 
@@ -392,78 +300,56 @@ public class DatafeedClientV2Test extends BotTest {
     }
   }
 
-  @Ignore
   @Test(expected = APIClientErrorException.class)
-  public void readDatafeedFailure400() {
-    stubFor(get(urlEqualTo(AgentConstants.READDATAFEED.replace("{id}", "1")))
-        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-        .willReturn(aResponse()
-            .withStatus(400)
-            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-            .withBody("{}")));
+  public void readDatafeedFailure400() throws IOException {
+    stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "1"),
+        readResourceContent("/response_content/datafeedv2/read_datafeedv5.json"), 400);
 
     assertNotNull(datafeedClient);
 
     final List<DatafeedEvent> events = datafeedClient.readDatafeed("1");
   }
 
-  @Ignore
   @Test(expected = SymClientException.class)
-  public void readDatafeedFailure401() {
-    stubFor(get(urlEqualTo(AgentConstants.READDATAFEED.replace("{id}", "1")))
-        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-        .willReturn(aResponse()
-            .withStatus(401)
-            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-            .withBody("{}")));
+  public void readDatafeedFailure401() throws IOException {
+    stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "1"),
+        readResourceContent("/response_content/datafeedv2/read_datafeedv5.json"), 401);
 
     assertNotNull(datafeedClient);
 
     final List<DatafeedEvent> events = datafeedClient.readDatafeed("1");
   }
 
-  @Ignore
   @Test(expected = ForbiddenException.class)
-  public void readDatafeedFailure403() {
-    stubFor(get(urlEqualTo(AgentConstants.READDATAFEED.replace("{id}", "1")))
-        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-        .willReturn(aResponse()
-            .withStatus(403)
-            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-            .withBody("{"
-                + "\"code\": 403,"
-                + "\"message\": \"The user lacks the required entitlement to perform this operation\"}")));
+  public void readDatafeedFailure403() throws IOException {
+    stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "1"),
+        readResourceContent("/response_content/datafeedv2/read_datafeedv5.json"), 403);
 
     assertNotNull(datafeedClient);
 
     final List<DatafeedEvent> events = datafeedClient.readDatafeed("1");
   }
 
-  @Ignore
   @Test(expected = ServerErrorException.class)
-  public void readDatafeedFailure500() {
-    stubFor(get(urlEqualTo(AgentConstants.READDATAFEED.replace("{id}", "1")))
-        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-        .willReturn(aResponse()
-            .withStatus(500)
-            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-            .withBody("{}")));
+  public void readDatafeedFailure500() throws IOException {
+    stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "1"),
+        readResourceContent("/response_content/datafeedv2/read_datafeedv5.json"), 500);
 
     assertNotNull(datafeedClient);
 
     final List<DatafeedEvent> events = datafeedClient.readDatafeed("1");
   }
 
-  @Ignore
+
   @Test(expected = javax.ws.rs.ProcessingException.class)
-  public void readDatafeedConnectionTimeout() {
-    stubFor(get(urlEqualTo(AgentConstants.READDATAFEED.replace("{id}", "1")))
-        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-        .willReturn(aResponse().withStatus(200).withFixedDelay(10000)));
+  public void readDatafeedConnectionTimeout() throws IOException{
+    stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "1"),
+        readResourceContent("/response_content/datafeedv2/read_datafeedv5.json"), 200, 10000);
 
     try {
 
       datafeedClient.readDatafeed("1");
+      assertTrue(true);
 
     } catch (SymClientException e) {
       fail();
@@ -472,39 +358,39 @@ public class DatafeedClientV2Test extends BotTest {
   // End readDatafeed
 
   // listDatafeedId
-    @Test
-    public void listDatafeedIdsSuccess() throws IOException {
-        stubGet(AgentConstants.LISTDATAFEEDV2,
-                readResourceContent("/response_content/datafeedv2/list_datafeedv2.json"));
+  @Test
+  public void listDatafeedIdsSuccess() throws IOException {
+    stubGet(AgentConstants.LISTDATAFEEDV2,
+        readResourceContent("/response_content/datafeedv2/list_datafeedv2.json"));
 
-      try {
+    try {
 
-        assertNotNull(datafeedClient);
+      assertNotNull(datafeedClient);
 
-        final List<DatafeedV2> datafeedIds = datafeedClient.listDatafeedId();
-        assertNotNull(datafeedIds);
+      final List<DatafeedV2> datafeedIds = datafeedClient.listDatafeedId();
+      assertNotNull(datafeedIds);
 
-        assertEquals(3, datafeedIds.size());
+      assertEquals(3, datafeedIds.size());
 
-        final List<String> ids = new ArrayList(Arrays.asList(
-                                      "2c2e8bb339c5da5711b55e32ba7c4687_f",
-                                      "4dd10564ef289e053cc59b2092080c3b_f",
-                                      "83b69942b56288a14d8625ca2c85f264_f"));
+      final List<String> ids = new ArrayList(Arrays.asList(
+          "2c2e8bb339c5da5711b55e32ba7c4687_f",
+          "4dd10564ef289e053cc59b2092080c3b_f",
+          "83b69942b56288a14d8625ca2c85f264_f"));
 
-        final  List<Long> createdAts = new ArrayList(Arrays.asList(
-                                            1536346282592L,
-                                            1536346282592L,
-                                            1536346282592L));
+      final  List<Long> createdAts = new ArrayList(Arrays.asList(
+          1536346282592L,
+          1536346282592L,
+          1536346282592L));
 
-        for(int i = 0; i < 3; i++) {
+      for(int i = 0; i < 3; i++) {
 
-          assertEquals(ids.get(i), datafeedIds.get(i).getId());
-          assertEquals(createdAts.get(i).longValue(), datafeedIds.get(i).getCreatedAt().longValue());
-        }
-      } catch (SymClientException e) {
-        fail();
+        assertEquals(ids.get(i), datafeedIds.get(i).getId());
+        assertEquals(createdAts.get(i).longValue(), datafeedIds.get(i).getCreatedAt().longValue());
       }
+    } catch (SymClientException e) {
+      fail();
     }
+  }
 
   @Test(expected = APIClientErrorException.class)
   public void listDatafeedIdsFailure400() {
@@ -547,27 +433,27 @@ public class DatafeedClientV2Test extends BotTest {
 
     final List<DatafeedV2> datafeedIds = datafeedClient.listDatafeedId();
   }
-    // End listDatafeedId
+  // End listDatafeedId
 
   // deleteDatafeed
   @Test
-    public void deleteDatafeedSuccess() {
-      stubFor(delete(urlEqualTo(AgentConstants.DELETEDATAFEEDV2.replace("{id}", "1")))
-          .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
-          .willReturn(aResponse()
-              .withStatus(204)
-              .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-              .withBody("{"
-                  + "\"code\": 204,"
-                  + "\"message\": \"Datafeed deleted with success.\""
-                  + "}")));
+  public void deleteDatafeedSuccess() {
+    stubFor(delete(urlEqualTo(AgentConstants.DELETEDATAFEEDV2.replace("{id}", "1")))
+        .withHeader(HttpHeaders.ACCEPT, equalTo(MediaType.APPLICATION_JSON))
+        .willReturn(aResponse()
+            .withStatus(204)
+            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+            .withBody("{"
+                + "\"code\": 204,"
+                + "\"message\": \"Datafeed deleted with success.\""
+                + "}")));
 
-      assertNotNull(datafeedClient);
+    assertNotNull(datafeedClient);
 
-      datafeedClient.deleteDatafeed("1");
+    datafeedClient.deleteDatafeed("1");
 
-      assertTrue(true);
-    }
+    assertTrue(true);
+  }
 
   @Test(expected = APIClientErrorException.class)
   public void deleteDatafeedFailure400() {
@@ -619,24 +505,24 @@ public class DatafeedClientV2Test extends BotTest {
   // End deleteDatafeed
 
   // getAckId
-    @Test
-    public void getAckIdSuccess() throws IOException {
-      stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "21449143d35a86461e254d28697214b4_f"),
-          readResourceContent("/response_content/datafeedv2/read_datafeedv2_old.json"), 200);
+  @Test
+  public void getAckIdSuccess() throws IOException {
+    stubPost(AgentConstants.READDATAFEEDV2.replace("{id}", "21449143d35a86461e254d28697214b4_f"),
+        readResourceContent("/response_content/datafeedv2/read_datafeedv2_old.json"), 200);
 
-      try {
+    try {
 
-        assertNotNull(datafeedClient);
+      assertNotNull(datafeedClient);
 
-        final List<DatafeedEvent> datafeedEvents = datafeedClient.readDatafeed("21449143d35a86461e254d28697214b4_f");
-        assertNotNull(datafeedEvents);
+      final List<DatafeedEvent> datafeedEvents = datafeedClient.readDatafeed("21449143d35a86461e254d28697214b4_f");
+      assertNotNull(datafeedEvents);
 
-        final String ackId = datafeedClient.getAckId();
-        assertEquals("ack_id_string", ackId);
+      final String ackId = datafeedClient.getAckId();
+      assertEquals("ack_id_string", ackId);
 
-      } catch (SymClientException e) {
-        fail();
-      }
+    } catch (SymClientException e) {
+      fail();
     }
-    // End getAckId
+  }
+  // End getAckId
 }
