6d6017be9f7aca03aac68643c74d2710c264403f
Haiddi-Avencod <68892288+Haiddi-Avencod@users.noreply.github.com>
2020-09-22 12:15:49 +0200

    SDK-134 Remove @Ignore annotation from class authentication.SymExtensionAppRSAAuthTest (#236)
    * SDK-134 Remove @Ignore annotation from class SymExtensionAppAuthTest
    
    * SDK-134 : Remove @Ignore annotation from class authentication.SymExtensionAppRSAAuthTest

--- symphony-bdk-legacy/symphony-api-client-java/src/test/java/authentication/SymExtensionAppAuthTest.java
+++ symphony-bdk-legacy/symphony-api-client-java/src/test/java/authentication/SymExtensionAppAuthTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@Ignore
 public class SymExtensionAppAuthTest extends BotTest {
     private SymExtensionAppAuth symExtensionAppAuth;
 
