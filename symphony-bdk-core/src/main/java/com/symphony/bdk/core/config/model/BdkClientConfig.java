3b0efc90a2cc402d36867e6927b6045056eb9154
Youri Bonnaffé <63661676+symphony-youri@users.noreply.github.com>
2020-09-25 17:39:47 +0200

    APP-3072: Load parent URL configuration with Spring Boot (#243)
    When Spring Boot was used the overriding mechanism we introduced where
    you can define the host, scheme, port and context at the top level and
    also override it for each sub component (agent, pod, ...) was not used.
    
    The logic has been builtin directly in the POJOs so it is always applied
    either when we use Jackson to deserialize or the Spring Boot
    configuration framework.

--- symphony-bdk-core/src/main/java/com/symphony/bdk/core/config/model/BdkClientConfig.java
+++ symphony-bdk-core/src/main/java/com/symphony/bdk/core/config/model/BdkClientConfig.java
@@ -3,43 +3,75 @@ package com.symphony.bdk.core.config.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.function.Supplier;
+
 @Getter
 @Setter
 public class BdkClientConfig {
 
-    private String scheme = null;
-    private String host = null;
-    private Integer port = null;
-    private String context = null;
+  private BdkConfig parentConfig;
 
-    private String proxyUrl = null;
-    private String proxyUsername = null;
-    private String proxyPassword = null;
+  private String scheme = null;
+  private String host = null;
+  private Integer port = null;
+  private String context = null;
 
-    private Integer connectTimeout = null;
-    private Integer readTimeout = null;
-    private Integer connectionRequestTimeout = null;
+  private String proxyUrl = null;
+  private String proxyUsername = null;
+  private String proxyPassword = null;
 
+  private Integer connectTimeout = null;
+  private Integer readTimeout = null;
+  private Integer connectionRequestTimeout = null;
 
-    public String getBasePath() {
-        return this.scheme + "://" + this.host + this.getPortAsString() + this.getFormattedContext();
+  public BdkClientConfig() {
+    // for Jackson deserialization
+  }
+
+  public BdkClientConfig(BdkConfig parentConfig) {
+    this.parentConfig = parentConfig;
+  }
+
+  public String getScheme() {
+    return thisOrParent(scheme, parentConfig::getScheme);
+  }
+
+  public String getHost() {
+    return thisOrParent(host, parentConfig::getHost);
+  }
+
+  public Integer getPort() {
+    return thisOrParent(port, parentConfig::getPort);
+  }
+
+  public String getContext() {
+    return thisOrParent(context, parentConfig::getContext);
+  }
+
+  private <T> T thisOrParent(T thisValue, Supplier<T> parentValue) {
+    return thisValue == null ? parentValue.get() : thisValue;
+  }
+
+  public String getBasePath() {
+    return this.getScheme() + "://" + this.getHost() + this.getPortAsString() + this.getFormattedContext();
+  }
+
+  public String getFormattedContext() {
+    final String localContext = this.getContext();
+    if (localContext == null) {
+      return "";
+    }
+    if (!localContext.equals("") && localContext.charAt(0) != '/') {
+      return "/" + localContext;
+    }
+    if (!localContext.equals("") && localContext.endsWith("/")) {
+      return localContext.substring(0, localContext.length() - 1);
     }
 
-    public String getFormattedContext() {
-        if (this.context == null) {
-            return "";
-        }
-        if (!this.context.equals("") && this.context.charAt(0) != '/') {
-            this.context =  "/" + this.context;
-        }
-        if (!this.context.equals("") && this.context.endsWith("/")) {
-            this.context = this.context.substring(0, this.context.length() - 1);
-        }
+    return localContext;
+  }
 
-        return this.context;
-    }
-
-    private String getPortAsString() {
-        return this.port != null ? ":" + this.port : "";
-    }
+  private String getPortAsString() {
+    return this.getPort() != null ? ":" + this.getPort() : "";
+  }
 }
