c5a93b3e908751333b484a9b6252d30f2b830fdf
symphony-thibault <thibault.pensec@symphony.com>
2020-09-28 18:14:21 +0200

    APP-3096 Added more checks

--- symphony-bdk-core/src/test/java/com/symphony/bdk/core/arch/CoreArchitectureTest.java
+++ symphony-bdk-core/src/test/java/com/symphony/bdk/core/arch/CoreArchitectureTest.java
@@ -1,17 +1,34 @@
 package com.symphony.bdk.core.arch;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
+import static com.tngtech.archunit.lang.conditions.ArchConditions.dependOnClassesThat;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
+import static com.tngtech.archunit.library.GeneralCodingRules.*;
 
 import com.symphony.bdk.core.service.user.mapper.UserDetailMapper;
 
+import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import org.apiguardian.api.API;
+import org.slf4j.Logger;
 
 /**
  * Validate basic architecture rules using <a href="https://www.archunit.org/getting-started">ArchUnit</a>.
+ *
+ * <p>
+ *   Rules currently checked:
+ *   <ul>
+ *     <li>all classes must be annotated with @API</li>
+ *     <li>loggers must be 'private static final'</li>
+ *     <li>Guava usage is not allowed (at the moment). This way we can control when and if we want to import it</li>
+ *     <li>usage of System.out and System.err is not allowed</li>
+ *     <li>usage of java.util.logging is not allowed</li>
+ *   </ul>
+ * </p>
  */
 @AnalyzeClasses(
     packages = "com.symphony.bdk.core",
@@ -32,5 +49,39 @@ public class CoreArchitectureTest {
         .should()
           .beAnnotatedWith(API.class)
     .check(classes);
+  }
+
+  @ArchTest
+  void loggers_should_be_private_static_final(JavaClasses classes) {
+    fields()
+        .that()
+          .haveRawType(Logger.class)
+        .should()
+          .bePrivate()
+        .andShould()
+          .beStatic()
+        .andShould()
+          .beFinal()
+    .check(classes);
+  }
+
+  public static final ArchCondition<JavaClass> USE_GUAVA =
+      dependOnClassesThat(resideInAPackage("com.google.guava"))
+          .as("use Guava");
+
+  @ArchTest
+  void no_guava(JavaClasses classes) {
+    noClasses().should(USE_GUAVA)
+        .because("Guava is not allowed at the moment").check(classes);
+  }
+
+  @ArchTest
+  void no_access_to_standard_streams(JavaClasses classes) {
+    NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS.check(classes);
+  }
+
+  @ArchTest
+  void no_java_util_logging(JavaClasses classes) {
+    NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING.check(classes);
   }
 }
