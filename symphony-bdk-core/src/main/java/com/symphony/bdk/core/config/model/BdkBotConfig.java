9cda4b4f16df797ad1c2ee524c92253d8629ada5
Thibault Pensec <39826516+symphony-thibault@users.noreply.github.com>
2020-09-29 10:05:37 +0200

    APP-3096 ArchUnit for Core module (#247)
    * APP-3096 ArchUnit ensures that classes are annotated with API Guardian
    
    * APP-3096 Added more checks

--- symphony-bdk-core/src/main/java/com/symphony/bdk/core/config/model/BdkBotConfig.java
+++ symphony-bdk-core/src/main/java/com/symphony/bdk/core/config/model/BdkBotConfig.java
@@ -2,9 +2,11 @@ package com.symphony.bdk.core.config.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apiguardian.api.API;
 
 @Getter
 @Setter
+@API(status = API.Status.STABLE)
 public class BdkBotConfig extends BdkAuthenticationConfig {
 
     private String username;
