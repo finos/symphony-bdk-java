fc56a5b71d6456088d19c6f6a6e7c57f733d1d4e
Elias Croze <elias.croze@symphony.com>
2020-11-10 15:12:00 +0100

    APP-3113: Throw exception in ServiceLookup if multiple implementations found in classpath

--- symphony-bdk-core/src/main/java/com/symphony/bdk/core/util/ServiceLookup.java
+++ symphony-bdk-core/src/main/java/com/symphony/bdk/core/util/ServiceLookup.java
@@ -18,19 +18,20 @@ public class ServiceLookup {
    * Load a service implementation class using {@link ServiceLoader}.
    *
    * @return a service implementation class.
+   * @throws IllegalStateException if no implementation or several implementations found in classpath.
    */
   public static <T> T lookupSingleService(Class<T> clz) {
 
     final ServiceLoader<T> classServiceLoader = ServiceLoader.load(clz);
 
     final List<T> services = StreamSupport.stream(classServiceLoader.spliterator(), false)
-            .collect(Collectors.toList());
+        .collect(Collectors.toList());
 
     if (services.isEmpty()) {
       throw new IllegalStateException("No service implementation found in classpath.");
     } else if (services.size() > 1) {
-      log.warn("More than 1 service implementation found in classpath, will use : {}",
-          services.get(0));
+      throw new IllegalStateException("More than 1 service implementation found in classpath for "
+          + clz.getCanonicalName());
     }
 
     return services.get(0);
