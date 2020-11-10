009a6c9f05e5afca44064b4f438510e0acf44177
Elias Croze <66254040+symphony-elias@users.noreply.github.com>
2020-11-10 18:15:16 +0100

    APP-3113: Throw exception if multiple implementations found in classpath for http-api and template-api (#340)
    * APP-3113: Throw exception in ServiceLookup if multiple implementations found in classpath
    
    * Throw exception when more than one TemplateEngine implementation found

--- symphony-bdk-core/src/main/java/com/symphony/bdk/core/util/ServiceLookup.java
+++ symphony-bdk-core/src/main/java/com/symphony/bdk/core/util/ServiceLookup.java
@@ -18,19 +18,21 @@ public class ServiceLookup {
    * Load a service implementation class using {@link ServiceLoader}.
    *
    * @return a service implementation class.
+   * @throws IllegalStateException if no implementation or several implementations found in classpath.
    */
   public static <T> T lookupSingleService(Class<T> clz) {
-
     final ServiceLoader<T> classServiceLoader = ServiceLoader.load(clz);
 
     final List<T> services = StreamSupport.stream(classServiceLoader.spliterator(), false)
-            .collect(Collectors.toList());
+        .collect(Collectors.toList());
 
     if (services.isEmpty()) {
-      throw new IllegalStateException("No service implementation found in classpath.");
+      throw new IllegalStateException(
+          "No service implementation found in classpath for " + clz.getCanonicalName() + ". "
+              + "Likely cause is a missing dependency to one of the symphony-bdk-http-* (jersey2 or webclient) modules.");
     } else if (services.size() > 1) {
-      log.warn("More than 1 service implementation found in classpath, will use : {}",
-          services.get(0));
+      throw new IllegalStateException("More than 1 service implementation found in classpath for "
+          + clz.getCanonicalName() + ". Try to remove a dependency to one of the symphony-bdk-http-* modules.");
     }
 
     return services.get(0);
