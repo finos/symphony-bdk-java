756533855f1e52483af24829646fc7f10aa9d3ce
Thibault Pensec <39826516+symphony-thibault@users.noreply.github.com>
2020-11-18 14:39:40 +0100

    APP-3287 Improved SlashAnnotationProcessor (backport from #347)
    * APP-3287 SlashAnnotationProcess scans singleton beans only
    
    * APP-3287 Updated doc
    
    * APP-3287 Created SlashAnnotationProcessor with more robust logic
    
    * APP-3287 Fixed code coverage
    
    * APP-3287 Fixed code coverage and updated javadoc
    
    * APP-3287 Fixed code coverage (hopefully final commit)
    
    * APP-3287 Fixed final review comments
    
    * APP-3287 Improved comments and error catching
    
    (cherry picked from commit 9dbe4438eda5552aaaf802fa06afd14c53be4769)

--- symphony-bdk-spring/symphony-bdk-core-spring-boot-starter/src/main/java/com/symphony/bdk/spring/config/BdkActivityConfig.java
+++ symphony-bdk-spring/symphony-bdk-core-spring-boot-starter/src/main/java/com/symphony/bdk/spring/config/BdkActivityConfig.java
@@ -1,28 +1,18 @@
 package com.symphony.bdk.spring.config;
 
-import static com.symphony.bdk.core.activity.command.SlashCommand.slash;
-
 import com.symphony.bdk.core.activity.AbstractActivity;
 import com.symphony.bdk.core.activity.ActivityRegistry;
-import com.symphony.bdk.core.activity.command.CommandContext;
 import com.symphony.bdk.core.auth.AuthSession;
 import com.symphony.bdk.core.service.SessionService;
 import com.symphony.bdk.core.service.datafeed.DatafeedService;
 import com.symphony.bdk.gen.api.model.UserV2;
 import com.symphony.bdk.spring.annotation.Slash;
+import com.symphony.bdk.spring.annotation.SlashAnnotationProcessor;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.aop.support.AopUtils;
-import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.AnnotationUtils;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
 
 /**
@@ -55,48 +45,5 @@ public class BdkActivityConfig {
   @Bean
   public SlashAnnotationProcessor slashAnnotationProcessor(ActivityRegistry registry) {
     return new SlashAnnotationProcessor(registry);
-  }
-
-  @Slf4j
-  @RequiredArgsConstructor
-  private static class SlashAnnotationProcessor implements ApplicationContextAware {
-
-    private final ActivityRegistry registry;
-
-    @Override
-    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
-
-      for (final String beanName : applicationContext.getBeanDefinitionNames()) {
-        final Object bean = applicationContext.getBean(beanName);
-
-        for (final Method m : getClass(bean).getDeclaredMethods()) {
-
-          final Slash annotation = AnnotationUtils.getAnnotation(m, Slash.class);
-
-          if (annotation != null) {
-            if (isMethodPrototypeValid(m)) {
-              this.registry.register(slash(annotation.value(), annotation.mentionBot(), c -> {
-                try {
-                  m.invoke(bean, c);
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                  log.error("Unable to invoke @Slash method {} from bean {}", m.getName(), bean.getClass(), e);
-                }
-              }));
-            } else {
-              log.warn("Method '{}' is annotated by @Slash but does not respect the expected prototype. "
-                  + "It must accept a single argument of type '{}'", m, CommandContext.class);
-            }
-          }
-        }
-      }
-    }
-
-    private static boolean isMethodPrototypeValid(Method m) {
-      return m.getParameterCount() == 1 && m.getParameters()[0].getType().equals(CommandContext.class);
-    }
-
-    private static Class<?> getClass(Object bean) {
-      return AopUtils.isAopProxy(bean) ? AopUtils.getTargetClass(bean) : bean.getClass();
-    }
   }
 }
