20f8fd581a6999d0a2ad21866d0379ca525c6631
haiddi <hserraye@avencod.fr>
2020-11-20 12:08:31 +0100

    SDK-170 Java BDK: Create test coverage for com.symphony.bdk.bot.sdk.monitoring.SymphonyHealthIndicator (verify status)

--- symphony-bdk-legacy/symphony-bdk-bot-sdk-java/src/test/java/com/symphony/bdk/bot/sdk/monitoring/SymphonyHealthIndicatorTest.java
+++ symphony-bdk-legacy/symphony-bdk-bot-sdk-java/src/test/java/com/symphony/bdk/bot/sdk/monitoring/SymphonyHealthIndicatorTest.java
@@ -4,76 +4,67 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import com.symphony.bdk.bot.sdk.lib.restclient.RestClient;
-import com.symphony.bdk.bot.sdk.lib.restclient.RestClientConnectionException;
-import com.symphony.bdk.bot.sdk.lib.restclient.RestClientImpl;
 import com.symphony.bdk.bot.sdk.symphony.HealthcheckClientImpl;
+import com.symphony.bdk.bot.sdk.symphony.model.HealthCheckInfo;
 
-import clients.SymBotClient;
-import configuration.SymConfig;
-import configuration.SymConfigLoader;
-import io.github.resilience4j.bulkhead.BulkheadConfig;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.actuate.health.Status;
 
-import java.time.Duration;
 import java.util.Map;
 
 public class SymphonyHealthIndicatorTest {
 
   private SymphonyHealthIndicator symphonyHealthIndicator;
   private HealthcheckClientImpl healthcheckClient;
-  private SymBotClient symBotClient;
-  private RestClient restClient;
+  private HealthCheckInfo healthCheckInfo;
 
   @Before
-  public void initSymphonyHealthIndicator(){
-    this.initRestClient();
-    this.initSymBotClient();
-    this.healthcheckClient = new HealthcheckClientImpl(this.symBotClient, this.restClient);
-    this.symphonyHealthIndicator = new SymphonyHealthIndicator(this.healthcheckClient);
+  public void initHealthCheck(){
+    this.healthcheckClient = Mockito.mock(HealthcheckClientImpl.class);
+    this.healthCheckInfo = Mockito.mock(HealthCheckInfo.class);
   }
 
   @Test
-  public void testHealth(){
+  public void testHealthDown(){
+    this.initSymphonyHealthIndicator(false);
+
+    final Health health = this.verifyHealthStatus(Status.DOWN);
+
+    verifyHealthDetailsContainSymphony(health);
+  }
+
+
+  @Test
+  public void testHealthUp(){
+    this.initSymphonyHealthIndicator(true);
+
+    final Health health = this.verifyHealthStatus(Status.UP);
+
+    verifyHealthDetailsContainSymphony(health);
+  }
+
+  //////// Private methods
+  private void initSymphonyHealthIndicator(final boolean overallStatus) {
+    Mockito.when(healthCheckInfo.checkOverallStatus()).thenReturn(overallStatus);
+
+    Mockito.when(this.healthcheckClient.healthCheck()).thenReturn(healthCheckInfo);
+
+    this.symphonyHealthIndicator = new SymphonyHealthIndicator(this.healthcheckClient);
+  }
+
+  private Health verifyHealthStatus(final Status status) {
     final Health health = this.symphonyHealthIndicator.health();
     assertNotNull(health);
+    assertEquals(status, health.getStatus());
+    return health;
+  }
 
+  private void verifyHealthDetailsContainSymphony(final Health health) {
     final Map<String, Object> healthDetails = health.getDetails();
     assertTrue(healthDetails.containsKey("Symphony"));
     assertEquals(this.healthcheckClient.healthCheck(), healthDetails.get("Symphony"));
-  }
-
-  /////// Private methods
-
-  private void initSymBotClient() {
-    final SymConfig symConfig = SymConfigLoader.loadConfig("src/test/resources/sym-config.json");
-
-    this.symBotClient = Mockito.mock(SymBotClient.class);
-    Mockito.when(this.symBotClient.getConfig()).thenReturn(symConfig);
-  }
-
-  private void initRestClient() {
-    final CircuitBreakerConfig cbConfig = CircuitBreakerConfig.custom()
-        .slidingWindowSize(20)
-        .waitDurationInOpenState(Duration.ofSeconds(30))
-        .recordExceptions(RestClientConnectionException.class)
-        .build();
-
-    assertNotNull(cbConfig);
-
-    final BulkheadConfig bhConfig = BulkheadConfig.custom()
-        .maxConcurrentCalls(30)
-        .maxWaitDuration(Duration.ofMillis(500))
-        .build();
-
-    assertNotNull(bhConfig);
-
-    this.restClient = new RestClientImpl(new RestTemplate(), cbConfig, bhConfig);
-    assertNotNull(this.restClient);
   }
 }
