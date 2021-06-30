22f14e6897554ca1c8b06712b0fd9e0037e1f1b7
Youri Bonnaffe <youri.bonnaffe@symphony.com>
2021-06-30 09:07:26 +0200

    Support client/response filters for HTTP clients
    The HTTP clients implementations supported by the BDK both support
    registering custom filters to intercept requests and responses. This can
    be useful to add custom logic, trace requests, provide metrics or
    support use cases such as Agentless.
    
    Using the SymphonyBdkBuilder it is now possible to register such
    filters.
    
    For instance:
    
    final SymphonyBdk bdk = new SymphonyBdkBuilder()
            .apiClientFactory(new ApiClientFactory(config){
              @Override
              protected ApiClientBuilder getApiClientBuilder(String basePath, BdkClientConfig clientConfig) {
                ApiClientBuilder apiClientBuilder = super.getApiClientBuilder(basePath, clientConfig);
                apiClientBuilder.addFilter(new JerseyCryptoFilter(config));
                return apiClientBuilder;
              }
            })
            .config(config)
            .build();
    
    The concrete implementation is specifc to each HTTP client, this is why
    the API takes a generic object.

--- symphony-bdk-http/symphony-bdk-http-webclient/src/main/java/com/symphony/bdk/http/webclient/ApiClientBuilderWebClient.java
+++ symphony-bdk-http/symphony-bdk-http-webclient/src/main/java/com/symphony/bdk/http/webclient/ApiClientBuilderWebClient.java
@@ -12,6 +12,7 @@ import org.apiguardian.api.API;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.transport.ProxyProvider;
@@ -23,7 +24,9 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -54,6 +57,7 @@ public class ApiClientBuilderWebClient implements ApiClientBuilder {
   protected int proxyPort;
   protected String proxyUser;
   protected String proxyPassword;
+  protected List<ExchangeFilterFunction> filters;
 
   public ApiClientBuilderWebClient() {
     this.basePath = "";
@@ -64,6 +68,7 @@ public class ApiClientBuilderWebClient implements ApiClientBuilder {
     this.proxyPort = -1;
     this.proxyUser = null;
     this.proxyPassword = null;
+    this.filters = new ArrayList<>();
     this.withUserAgent(ApiUtils.getUserAgent());
   }
 
@@ -72,9 +77,11 @@ public class ApiClientBuilderWebClient implements ApiClientBuilder {
    */
   @Override
   public ApiClient build() {
-    final WebClient webClient = WebClient.builder()
+    WebClient.Builder builder = WebClient.builder()
         .clientConnector(new ReactorClientHttpConnector(this.createHttpClient()))
-        .baseUrl(this.basePath)
+        .baseUrl(this.basePath);
+    this.filters.forEach(builder::filter);
+    final WebClient webClient = builder
         .build();
 
     return new ApiClientWebClient(webClient, this.basePath, this.defaultHeaders);
@@ -171,6 +178,18 @@ public class ApiClientBuilderWebClient implements ApiClientBuilder {
   public ApiClientBuilder withProxyCredentials(String proxyUser, String proxyPassword) {
     this.proxyUser = proxyUser;
     this.proxyPassword = proxyPassword;
+    return this;
+  }
+
+  @Override
+  public ApiClientBuilder addFilter(Object filter) {
+    if (!(filter instanceof ExchangeFilterFunction)) {
+      throw new IllegalArgumentException(
+          "The filter " + filter.getClass().getName()
+              + " must be an instance of " + ExchangeFilterFunction.class.getName()
+              + " to be used with WebClient HTTP client");
+    }
+    this.filters.add((ExchangeFilterFunction) filter);
     return this;
   }
 
