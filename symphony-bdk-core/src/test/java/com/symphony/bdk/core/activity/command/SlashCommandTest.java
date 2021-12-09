082c55bb0ccab1758e2e595829e521c60d843172
Elias Croze <elias.croze@symphony.com>
2021-12-09 13:29:07 +0100

    Added tests

--- symphony-bdk-core/src/test/java/com/symphony/bdk/core/activity/command/SlashCommandTest.java
+++ symphony-bdk-core/src/test/java/com/symphony/bdk/core/activity/command/SlashCommandTest.java
@@ -81,8 +81,52 @@ class SlashCommandTest {
   }
 
   @Test
+  void testCommandNotTriggeredOnMismatchingBotId() {
+    final AtomicBoolean handlerCalled = new AtomicBoolean(false);
+    final Consumer<CommandContext> handler = c -> handlerCalled.set(true);
+
+    final RealTimeEventsProvider provider = new RealTimeEventsProvider();
+    final SlashCommand cmd = SlashCommand.slash("/test", handler);
+    cmd.setBotDisplayName(BOT_DISPLAY_NAME);
+    cmd.setBotUserId(BOT_USER_ID);
+    cmd.bindToRealTimeEventsSource(provider::setListener);
+
+    final V4MessageSent event = createMessageSentEvent("<div><p>"
+            + "<span class=\"entity\" data-entity-id=\"0\">@" + BOT_DISPLAY_NAME + "</span>"
+            + "/text"
+            + "</p></div>",
+        "{\"0\":{\"id\":[{\"type\":\"com.symphony.user.userId\",\"value\":\"99999\"}],\"type\":\"com.symphony.user.mention\"}}");
+
+    provider.trigger(l -> l.onMessageSent(new V4Initiator(), event));
+
+    assertFalse(handlerCalled.get());
+  }
+
+  @Test
+  void testCommandNotTriggeredWithCorrectTextButNotMention() {
+    final AtomicBoolean handlerCalled = new AtomicBoolean(false);
+    final Consumer<CommandContext> handler = c -> handlerCalled.set(true);
+
+    final RealTimeEventsProvider provider = new RealTimeEventsProvider();
+    final SlashCommand cmd = SlashCommand.slash("/test", handler);
+    cmd.setBotDisplayName(BOT_DISPLAY_NAME);
+    cmd.setBotUserId(BOT_USER_ID);
+    cmd.bindToRealTimeEventsSource(provider::setListener);
+
+    final V4MessageSent event = createMessageSentEvent("<div><p>"
+            + "@" + BOT_DISPLAY_NAME + " " + "/text"
+            + "</p></div>",
+        "{}");
+
+    provider.trigger(l -> l.onMessageSent(new V4Initiator(), event));
+
+    assertFalse(handlerCalled.get());
+  }
+
+  @Test
   void testVerifyBotInfo() {
-    final SlashCommand cmd = SlashCommand.slash("/test", c -> {});
+    final SlashCommand cmd = SlashCommand.slash("/test", c -> {
+    });
     final ActivityInfo info = cmd.getInfo();
     assertEquals(ActivityType.COMMAND, info.type());
     assertEquals("/test", info.name());
@@ -102,15 +146,22 @@ class SlashCommandTest {
   }
 
   private static V4MessageSent createMessageSentEvent(boolean botMention, String slashCommand) {
+    String botMentionString =
+        botMention ? "<span class=\"entity\" data-entity-id=\"0\">@" + BOT_DISPLAY_NAME + "</span> " : "";
+    String message = "<div><p>" + botMentionString + slashCommand + "</p></div>";
+
+    String data = botMention ? "{\"0\":{\"id\":[{\"type\":\"com.symphony.user.userId\",\"value\":\"" + BOT_USER_ID
+        + "\"}],\"type\":\"com.symphony.user.mention\"}}" : "{}";
+
+    return createMessageSentEvent(message, data);
+  }
+
+  private static V4MessageSent createMessageSentEvent(String message, String data) {
     final V4MessageSent event = new V4MessageSent().message(new V4Message().stream(new V4Stream()));
     event.getMessage().getStream().setStreamId(UUID.randomUUID().toString());
     event.getMessage().setMessageId(UUID.randomUUID().toString());
 
-    String botMentionString = botMention ? "<span class=\"entity\" data-entity-id=\"0\">@" + BOT_DISPLAY_NAME + "</span> " : "";
-    event.getMessage().setMessage("<div><p>" + botMentionString + slashCommand + "</p></div>");
-
-    String data = botMention ? "{\"0\":{\"id\":[{\"type\":\"com.symphony.user.userId\",\"value\":\"" + BOT_USER_ID
-        + "\"}],\"type\":\"com.symphony.user.mention\"}}" : "{}";
+    event.getMessage().setMessage(message);
     event.getMessage().setData(data);
     return event;
   }
