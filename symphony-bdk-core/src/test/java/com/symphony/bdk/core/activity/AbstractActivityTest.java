4155cc40602786cfbbea5218a1135f2993f78ed0
Thibault Pensec <39826516+symphony-thibault@users.noreply.github.com>
2022-01-11 14:41:29 +0100

    #621 Added streamId and formMessageId to FormReplyActivity context (#622)
    * #621 Added streamId and formMessageId to FormReplyActivity
    
    * #621 Fixed AbstractActivityTest
    
    * #621 Fixed Javadoc

--- symphony-bdk-core/src/test/java/com/symphony/bdk/core/activity/AbstractActivityTest.java
+++ symphony-bdk-core/src/test/java/com/symphony/bdk/core/activity/AbstractActivityTest.java
@@ -1,10 +1,11 @@
 package com.symphony.bdk.core.activity;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.symphony.bdk.core.activity.form.TestFormReplyActivity;
 import com.symphony.bdk.core.service.datafeed.EventException;
 import com.symphony.bdk.gen.api.model.V4Initiator;
+import com.symphony.bdk.gen.api.model.V4Stream;
 import com.symphony.bdk.gen.api.model.V4SymphonyElementsAction;
 
 import org.junit.jupiter.api.Test;
@@ -35,7 +36,7 @@ class AbstractActivityTest {
     });
 
     assertThrows(EventException.class,
-        () -> act.processEvent(new V4Initiator(), new V4SymphonyElementsAction()));
+        () -> act.processEvent(new V4Initiator(), new V4SymphonyElementsAction().stream(new V4Stream())));
   }
 
   @Test
