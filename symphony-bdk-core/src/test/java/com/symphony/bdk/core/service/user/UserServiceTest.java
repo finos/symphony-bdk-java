78f1fc4d949b77dcbbdc0b96b136552eb98acf9f
Elias Croze <elias.croze@symphony.com>
2022-01-26 18:05:34 +0100

    Addded unit test coverage

--- symphony-bdk-core/src/test/java/com/symphony/bdk/core/service/user/UserServiceTest.java
+++ symphony-bdk-core/src/test/java/com/symphony/bdk/core/service/user/UserServiceTest.java
@@ -829,6 +829,25 @@ class UserServiceTest {
   }
 
   @Test
+  void listAllUserFollowersNoPaginationTest() {
+    this.mockApiClient.onGet(V1_LIST_FOLLOWERS.replace("{uid}", "1234"), "{\n"
+        + "    \"count\": 3,\n"
+        + "    \"followers\": [\n"
+        + "        13056700579848,\n"
+        + "        13056700580889,\n"
+        + "        13056700580890\n"
+        + "    ]\n"
+        + "}");
+
+    List<Long> followers = this.service.listAllUserFollowers(1234L).collect(Collectors.toList());
+
+    assertEquals(followers.size(), 3);
+    assertEquals(followers.get(0), 13056700579848L);
+    assertEquals(followers.get(1), 13056700580889L);
+    assertEquals(followers.get(2), 13056700580890L);
+  }
+
+  @Test
   void listAllUserFollowersPaginationTest() {
     this.mockApiClient.onGet(V1_LIST_FOLLOWERS.replace("{uid}", "1234"), "{\n"
         + "    \"count\": 3,\n"
@@ -945,6 +964,24 @@ class UserServiceTest {
   }
 
   @Test
+  void listAllUserFollowingsPaginationNoPaginationTest() {
+    this.mockApiClient.onGet(V1_LIST_FOLLOWING.replace("{uid}", "1234"),
+        "{\n"
+            + "    \"count\": 2,\n"
+            + "    \"following\": [\n"
+            + "        13056700580888,\n"
+            + "        13056700580889\n"
+            + "    ]\n"
+            + "}");
+
+    List<Long> followers = this.service.listAllUserFollowing(1234L, new StreamPaginationAttribute(10, 10)).collect(Collectors.toList());
+
+    assertEquals(followers.size(), 2);
+    assertEquals(followers.get(0), 13056700580888L);
+    assertEquals(followers.get(1), 13056700580889L);
+  }
+
+  @Test
   void userDetailMapperNullTest() {
     V2UserDetail userDetailNull = UserDetailMapper.INSTANCE.userDetailToV2UserDetail(null);
 
@@ -1018,12 +1055,24 @@ class UserServiceTest {
   }
 
   @Test
-  void listAllAuditTrail() throws IOException {
+  void listAllAuditTrailNoPagination() throws IOException {
     String response = JsonHelper.readFromClasspath("/audit_trail/audit_trail_initiator_list_v1.json");
     this.mockApiClient.onGet(V1_AUDIT_TRAIL_PRIVILEGED_USER, response);
 
     final List<V1AuditTrailInitiatorResponse> auditTrails =
-        this.service.listAllAuditTrail(1551888601279L, 1551888601279L, 1353716993L, "SUPER_ADMINISTRATOR", 2, 2)
+        this.service.listAllAuditTrail(1551888601279L, 1551888601279L, 1353716993L, "SUPER_ADMINISTRATOR", 2, 3)
+            .collect(Collectors.toList());
+
+    assertEquals(auditTrails.size(), 2);
+  }
+
+  @Test
+  void listAllAuditTrail() throws IOException {
+    String response = JsonHelper.readFromClasspath("/audit_trail/audit_trail_initiator_list_v1_pagination.json");
+    this.mockApiClient.onGet(V1_AUDIT_TRAIL_PRIVILEGED_USER, response);
+
+    final List<V1AuditTrailInitiatorResponse> auditTrails =
+        this.service.listAllAuditTrail(1551888601279L, 1551888601279L, 1353716993L, "SUPER_ADMINISTRATOR", 2, 3)
             .collect(Collectors.toList());
 
     assertEquals(auditTrails.size(), 2);
