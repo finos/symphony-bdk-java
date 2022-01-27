f1718e0d20c011233ecd618e1ec959431a886d5a
Elias Croze <elias.croze@symphony.com>
2022-01-27 14:22:41 +0100

    Simplified OffsetBasedPaginationService

--- symphony-bdk-core/src/main/java/com/symphony/bdk/core/service/pagination/OffsetBasedPaginatedService.java
+++ symphony-bdk-core/src/main/java/com/symphony/bdk/core/service/pagination/OffsetBasedPaginatedService.java
@@ -5,6 +5,7 @@ import com.symphony.bdk.http.api.ApiRuntimeException;
 
 import org.apiguardian.api.API;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -47,48 +48,46 @@ public class OffsetBasedPaginatedService<T> extends PaginatedService {
   @API(status = API.Status.INTERNAL)
   private class PaginatedIterator implements Iterator<T> {
 
-    private List<T> currentChunk;
-    private int remainingItemsInChunk;
-    private int currentIndexInChunk;
-    private int currentIndexInTotal;
+    private int currentChunkSize;
+    private List<T> remainingItemsInChunk;
+    private int fetchedItems;
     private int currentOffset;
 
     public PaginatedIterator() {
-      currentChunk = Collections.emptyList();
-      currentIndexInChunk = -1;
-      currentIndexInTotal = -1;
-      remainingItemsInChunk = 0;
+      currentChunkSize = -1;
+      remainingItemsInChunk = Collections.emptyList();
+      fetchedItems = 0;
       currentOffset = 0;
     }
 
     @Override
     public boolean hasNext() {
-      //we already fetched maxSize items
-      if (currentIndexInTotal == maxSize - 1) {
+      // we already fetched maxSize items
+      if (fetchedItems == maxSize) {
         return false;
       }
 
-      //we didn't fetch maxSize items and we have remaining items in the chunk
-      if (remainingItemsInChunk != 0) {
+      // we didn't fetch maxSize items and we have remaining items in the chunk
+      if (!remainingItemsInChunk.isEmpty()) {
         return true;
       }
 
-      //no remaining items in chunk but chunk was already smaller than chunkSize: we are already at the end
-      if (currentIndexInTotal != -1 && currentChunk.size() < chunkSize) {
+      // no remaining items in chunk but chunk was already smaller than chunkSize: we are already at the end
+      if (fetchedItems > 0 && currentChunkSize < chunkSize) {
         return false;
       }
 
-      //no remaining items in chunk, let's fetch a new one
+      // no remaining items in chunk, let's fetch a new one
       fetchNewChunk();
 
-      return remainingItemsInChunk != 0;
+      return !remainingItemsInChunk.isEmpty();
     }
 
     private void fetchNewChunk() {
       try {
-        currentChunk = paginatedApi.get(currentOffset, chunkSize);
-        remainingItemsInChunk = currentChunk == null ? 0 : currentChunk.size();
-        currentIndexInChunk = -1;
+        final List<T> currentChunk = paginatedApi.get(currentOffset, chunkSize);
+        remainingItemsInChunk = currentChunk == null ? Collections.emptyList() : new ArrayList<>(currentChunk);
+        currentChunkSize = remainingItemsInChunk.size();
         currentOffset += chunkSize;
       } catch (ApiException e) {
         throw new ApiRuntimeException(e);
@@ -97,10 +96,8 @@ public class OffsetBasedPaginatedService<T> extends PaginatedService {
 
     @Override
     public T next() {
-      currentIndexInTotal++;
-      currentIndexInChunk++;
-      remainingItemsInChunk--;
-      return currentChunk.get(currentIndexInChunk);
+      fetchedItems++;
+      return remainingItemsInChunk.remove(0);
     }
   }
 
