7fe24fa0d6c2f49f8e0721a03d6da0db231379a5
Mariacristina De Dominicis <65179248+symphony-mariacristina@users.noreply.github.com>
2022-03-23 11:25:24 +0100

    Starting Datafeed v1 deprecation process (#643)
    * Starting Datafeed v1 deprecation process
    The datafeed 1 service will be fully replaced by the datafeed 2 service in the future. Please consider migrating over to datafeed 2. For more information on the timeline as well as on the benefits of datafeed 2, please reach out to your Technical Account Manager or to our developer documentation https://docs.developers.symphony.com/building-bots-on-symphony/datafeed
    This PR includes:
    - if datafeed version is not defined in the config, the BDK will use DFv2 by default
    - added deprecation notice when DFv1 is used (both in the code and in the documentation)
    - APIs generation is been updated following swagger deprecation of the APIs https://github.com/finos/symphony-api-spec/pull/143

--- symphony-bdk-config/src/main/java/com/symphony/bdk/core/config/model/BdkDatafeedConfig.java
+++ symphony-bdk-config/src/main/java/com/symphony/bdk/core/config/model/BdkDatafeedConfig.java
@@ -1,5 +1,7 @@
 package com.symphony.bdk.core.config.model;
 
+import static com.symphony.bdk.core.config.util.DeprecationLogger.logDeprecation;
+
 import lombok.Getter;
 import lombok.Setter;
 import org.apiguardian.api.API;
@@ -11,17 +13,27 @@ import java.io.File;
 @API(status = API.Status.STABLE)
 public class BdkDatafeedConfig {
 
-    private String version = "v1";
-    private String idFilePath;
-    private BdkRetryConfig retry = new BdkRetryConfig(BdkRetryConfig.INFINITE_MAX_ATTEMPTS);
+  private String version = "v2";
+  private String idFilePath;
+  private BdkRetryConfig retry = new BdkRetryConfig(BdkRetryConfig.INFINITE_MAX_ATTEMPTS);
 
-    public String getIdFilePath() {
-        if (idFilePath == null || idFilePath.isEmpty()) {
-            return "." + File.separator;
-        }
-        if (!idFilePath.endsWith(File.separator)) {
-            return idFilePath + File.separator;
-        }
-        return idFilePath;
+  public void setVersion(String version) {
+    if ("v1".equalsIgnoreCase(version)) {
+      logDeprecation("The datafeed 1 service will be fully replaced by the datafeed 2 service in the future. "
+          + "Please consider migrating over to datafeed 2. For more information on the timeline as well as on the "
+          + "benefits of datafeed 2, please reach out to your Technical Account Manager or to our developer "
+          + "documentation https://docs.developers.symphony.com/building-bots-on-symphony/datafeed)");
     }
+    this.version = version;
+  }
+
+  public String getIdFilePath() {
+    if (idFilePath == null || idFilePath.isEmpty()) {
+      return "." + File.separator;
+    }
+    if (!idFilePath.endsWith(File.separator)) {
+      return idFilePath + File.separator;
+    }
+    return idFilePath;
+  }
 }
